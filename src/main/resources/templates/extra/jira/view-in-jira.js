c3eeb8a0808ae038f6298d865a87fa2a8eca7a43
David Chui <dchui@atlassian.com>
2012-03-20 04:27:08 +0000

    CONFJIRA-227 Fixed "View in JIRA" panel.

--- src/main/resources/templates/extra/jira/view-in-jira.js
+++ src/main/resources/templates/extra/jira/view-in-jira.js
@@ -1,21 +1,22 @@
+
 AJS.bind("add-handler.property-panel", function(event, panel) {
     AJS.log("add-handler.property-panel: panel name = " + panel.name);
     if (panel.name != "macro")
         return;
-    
+
     panel.registerButtonHandler('view-in-jira', function(event, macroElement) {
         if (AJS.Editor.JiraConnector.servers) {
             var servers = AJS.Editor.JiraConnector.servers;
             //var macroHtml = AJS.Rte.getEditor().serializer.serialize(AJS.$(macroElement).clone()[0]);
             var $macroNode = AJS.$(macroElement);
-            
+
             //var windowName = (AJS.$.browser && AJS.$.browser.msie) ? "_blank" : "confluence-goto-link-include-macro-" + macroElement.id;
 
             var defaultParam = $macroNode.attr("data-macro-default-parameter");
             var parameters = $macroNode.attr("data-macro-parameters");
             //var macro = AJS.$.secureEvalJSON(macroData);
             var jql_operators = /=|!=|~|>|<|!~| is | in /i;
-           
+
             var parseUglyMacro = function(macroTxt){
                 //get first macro parameter and assume its a jql query
                 var bar = macroTxt.indexOf("|");
@@ -24,7 +25,7 @@ AJS.bind("add-handler.property-panel", function(event, panel) {
                 }
                 return macroTxt;
             };
-            var searchStr = defaultParam || macro.params["jqlQuery"] || macro.params["key"] || parseUglyMacro(parameters);
+            var searchStr = defaultParam || parameters["jqlQuery"] || parameters["key"] || parseUglyMacro(parameters);
             var paramArray = parameters.split("|");
             var serverName;
             for (var i = 0; i < paramArray.length; i++){
@@ -40,7 +41,7 @@ AJS.bind("add-handler.property-panel", function(event, panel) {
                 if ((serverName && servers[i].name == serverName) || (!serverName && servers[i].selected)){
                     server = servers[i];
                     break;
-                }                       
+                }
             }
 
             if (server) {
@@ -48,7 +49,7 @@ AJS.bind("add-handler.property-panel", function(event, panel) {
                 if (baseURL.charAt(baseURL.length - 1) == '/') {
                     baseURL = baseURL.substr(0, baseURL.length - 1);
                 }
-                
+
                 var windowName = tinymce.isIE ? "_blank" : "confluence-goto-jiralink-" + AJS.params.pageId + "-" +  Math.random();
                 if (!isJQL)
                     var win = window.open(baseURL + '/browse/' + encodeURIComponent(searchStr), windowName);
