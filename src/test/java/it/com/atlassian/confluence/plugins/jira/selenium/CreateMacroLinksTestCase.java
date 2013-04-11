665b5fd8c32fcd686ba83074493f1247c23e0c6c
hieu.ta <hta@atlassian.com>
2013-04-11 18:37:14 +0700

    add Integration test

---
+++ src/test/java/it/com/atlassian/confluence/plugins/jira/selenium/CreateMacroLinksTestCase.java
@@ -0,0 +1,152 @@
+package it.com.atlassian.confluence.plugins.jira.selenium;
+
+
+/**
+ * This class contains tests for JIRA remote issue linking.
+ */
+public class CreateMacroLinksTestCase extends AbstractJiraPanelTestCase
+{
+    
+    private static final String COUNT_PARAM = "count";
+    private static final String TABLE_PARAM = "table";
+    private static final String COLUMNS_PARAM = "columns";
+    
+    static String searchStr = "project = TP";
+	static String[] expected = new String[]{"TP-2", "TP-1"};
+	static String columns = "key, summary";
+	
+    @Override
+    public void setUp() throws Exception
+    {
+        super.setUp();
+    }
+
+    @Override
+    public void tearDown() throws Exception
+    {
+        super.tearDown();
+       // deleteRemoteLinks("TP-1");
+    }
+
+    /**
+     * create macro link and insert to RTE
+     */
+    public void testCreateLinkMacro() {
+    	openJiraDialog();
+    	String paramName = "";
+    	searchAndInsertLinkMacroWithParam(paramName, searchStr, expected);
+    }
+    
+    /**
+     * select option table in macro dialog and insert macro link in the Editor
+     */
+    public void testCreateLinkMacroWithParamTable() {
+    	openJiraDialog();
+    	String paramName = TABLE_PARAM;
+    	searchAndInsertLinkMacroWithParam(paramName, searchStr, expected);
+    }
+    
+    /**
+     * select option count in macro dialog and insert macro link in the Editor
+     */
+    public void testCreateLinkMacroWithParamCount() {
+    	openJiraDialog();
+    	String paramName = COUNT_PARAM;
+    	searchAndInsertLinkMacroWithParam(paramName, searchStr, expected);
+    	validateParamInLinkMacro("count=true");
+    }
+    
+    /**
+     * input value to columns field in macro dialog and insert macro link in the Editor
+     */
+    public void testCreateLinkMacroWithParamColumns() {
+    	openJiraDialog();
+    	String paramName = COLUMNS_PARAM;
+    	searchAndInsertLinkMacroWithParam(paramName, searchStr, expected);
+    	validateParamInLinkMacro("columns=key,summary");
+    }
+    
+    /**
+     * input value to columns field in macro dialog and insert macro link in the Editor, 
+     * create page with this macro and Edit page check macro param columns
+     */
+    public void testCreatePageWithParamColumnMacro() {
+    	//add link macro with column params
+    	openJiraDialog();
+    	
+    	String paramName = COLUMNS_PARAM;
+    	searchAndInsertLinkMacroWithParam(paramName, searchStr, expected);
+    	    	
+    	//add title for page
+    	client.click("css=#content-title");
+        final String contentId = client.getEval("window.AJS.Confluence.Editor.getContentId()");
+        client.typeKeys("css=#content-title", "Test " + contentId);
+
+        // Save page in default location
+        client.clickAndWaitForAjaxWithJquery("css=#rte-button-publish");
+        client.waitForPageToLoad();
+        
+        //click edit page
+        client.click("css=#editPageLink");
+        client.waitForPageToLoad();
+
+    	validateParamInLinkMacro("columns=key,summary");
+    }
+    
+    /**
+     * validate param in data-macro-parameters from the macro placeholder in the Editor
+     * @param paramMarco
+     */
+    private void validateParamInLinkMacro(String paramMarco) {
+    	String parameters = getJiraMacroParameters();
+    	assertTrue(parameters.contains(paramMarco));
+    }
+
+    private void searchAndInsertLinkMacroWithParam(String paramName, String searchStr, String... expected) {
+    	
+    	client.click("//li/button[text()='Search']");
+
+        client.type("css=input[name='jiraSearch']", searchStr);
+
+        client.clickAndWaitForAjaxWithJquery("css=div.jira-search-form button");
+        
+        for(int i = 0; i < expected.length; i++ ) {
+            assertEquals(expected[i], client.getTable("css=#my-jira-search table.my-result." + (i + 1) + ".1"));
+        }
+
+        //click to open option
+        client.click("css=#my-jira-search .jql-display-opts-open");
+        
+        if(paramName.equals(COUNT_PARAM)) {
+        	client.check("insert-advanced", "insert-count");
+        } 
+        
+        if(paramName.equals(TABLE_PARAM)) {
+        	client.check("insert-advanced", "insert-table");
+        }
+        
+        if(paramName.equals(COLUMNS_PARAM)) {
+        	client.check("insert-advanced", "insert-table");
+            client.type("css=input[name='columns-display']", columns);
+        }
+        
+        client.clickAndWaitForAjaxWithJquery("css=button.insert-issue-button", 3000);
+    }
+    
+    /**
+     * 
+     * @return the value of the data-macro-parameters attribute from the macro placeholder in the Editor. Only the first found macro
+     * is used.
+     */
+    private String getJiraMacroParameters()
+    {
+    	//look macro link in RTE
+        client.selectFrame("wysiwygTextarea_ifr");
+        // debug
+        assertThat.elementVisible("xpath=//img[@class='editor-inline-macro' and @data-macro-name='jira']");
+        String attributeValue = client.getAttribute("xpath=//img[@class='editor-inline-macro' and @data-macro-name='jira']/@data-macro-parameters");
+        client.selectFrame("relative=top");
+        return attributeValue;
+    }
+    
+}
