9d14bd62b88118882ea7be1c73c7ce2ab84db118
khoa.tran <khtran@atlassian.com>
2013-04-25 10:46:19 +0700

    Refactor the code of MacroPlaceHolderTestCase to remove duplicated code

--- src/test/java/it/com/atlassian/confluence/plugins/jira/selenium/MacroPlaceHolderTestCase.java
+++ src/test/java/it/com/atlassian/confluence/plugins/jira/selenium/MacroPlaceHolderTestCase.java
@@ -3,7 +3,26 @@ package it.com.atlassian.confluence.plugins.jira.selenium;
 import com.thoughtworks.selenium.Wait;
 
 public class MacroPlaceHolderTestCase extends AbstractJiraDialogTestCase {
-
+    
+    /**
+     * Get the image source attribute (src) of the macro place holder
+     * based on the XPath expression (ex: "xpath=//img[@class='editor-inline-macro' and @data-macro-name='jira']")
+     * @param xpathExp XPath string expression (start with: xpath=)
+     * @return image source attribute value
+     */
+    private String getImageSourceOfMacroElement(final String xpathExp) {
+        client.selectFrame("wysiwygTextarea_ifr");
+        Wait wait = new Wait("Checking Jira link") {
+            public boolean until() {
+                return client
+                        .isElementPresent(xpathExp);
+            }
+        };
+        wait.wait("Couldn't find new Jira link", 5000);
+        assertThat.elementVisible(xpathExp);
+        return client.getAttribute(xpathExp + "/@src");
+    }
+    
     public void testPlaceHolderWhenMacroContainsOneIssue() throws Exception {
         login();
 
@@ -12,21 +31,8 @@ public class MacroPlaceHolderTestCase extends AbstractJiraDialogTestCase {
         JiraConnectorDialog dialog = JiraConnectorDialog.openDialog(client);
         dialog.performSearch("TST-1").clickInsert();
         client.selectFrame("wysiwygTextarea_ifr");
-        Wait wait = new Wait("Checking Jira link") {
-            public boolean until() {
-                return client
-                        .isElementPresent("xpath=//img[@class='editor-inline-macro' and @data-macro-name='jira']");
-            }
-        };
-        wait.wait("Couldn't find new Jira link", 5000);
-
-        assertThat
-                .elementVisible("xpath=//img[@class='editor-inline-macro' and @data-macro-name='jira']");
-
-        String attributeValue = client
-                .getAttribute("xpath=//img[@class='editor-inline-macro' and @data-macro-name='jira']/@src");
-        assertTrue(attributeValue
-                .contains("/plugins/servlet/confluence/placeholder/macro"));
+        String imgSrcAttValueOfMacro = getImageSourceOfMacroElement("xpath=//img[@class='editor-inline-macro' and @data-macro-name='jira']");
+        assertTrue(imgSrcAttValueOfMacro.contains("/plugins/servlet/confluence/placeholder/macro"));
         client.selectFrame("relative=top");
     }
 
@@ -34,25 +40,10 @@ public class MacroPlaceHolderTestCase extends AbstractJiraDialogTestCase {
         login();
 
         client.open("pages/createpage.action?spaceKey=" + TEST_SPACE_KEY);
-
         JiraConnectorDialog dialog = JiraConnectorDialog.openDialog(client);
         dialog.performSearch("TSTT-1, TST-1").clickInsert();
-        client.selectFrame("wysiwygTextarea_ifr");
-        Wait wait = new Wait("Checking Jira link") {
-            public boolean until() {
-                return client
-                        .isElementPresent("xpath=//img[@class='editor-inline-macro' and @data-macro-name='jira']");
-            }
-        };
-        wait.wait("Couldn't find new Jira link", 5000);
-
-        assertThat
-                .elementVisible("xpath=//img[@class='editor-inline-macro' and @data-macro-name='jira']");
-
-        String attributeValue = client
-                .getAttribute("xpath=//img[@class='editor-inline-macro' and @data-macro-name='jira']/@src");
-        assertTrue(attributeValue
-                .contains("/confluence/download/resources/confluence.extra.jira/jira-table.png"));
+        String imgSrcAttValueOfMacro = getImageSourceOfMacroElement("xpath=//img[@class='editor-inline-macro' and @data-macro-name='jira']");
+        assertTrue(imgSrcAttValueOfMacro.contains("/confluence/download/resources/confluence.extra.jira/jira-table.png"));
         client.selectFrame("relative=top");
     }
 
@@ -65,21 +56,8 @@ public class MacroPlaceHolderTestCase extends AbstractJiraDialogTestCase {
         JiraConnectorDialog dialog = JiraConnectorDialog.openDialog(client);
         dialog.performSearch("project = 'Alphanumeric Key Test'").clickInsert();
         client.selectFrame("wysiwygTextarea_ifr");
-        Wait wait = new Wait("Checking Jira link") {
-            public boolean until() {
-                return client
-                        .isElementPresent("xpath=//img[@class='editor-inline-macro' and @data-macro-name='jira']");
-            }
-        };
-        wait.wait("Couldn't find new Jira link", 5000);
-
-        assertThat
-                .elementVisible("xpath=//img[@class='editor-inline-macro' and @data-macro-name='jira']");
-
-        String attributeValue = client
-                .getAttribute("xpath=//img[@class='editor-inline-macro' and @data-macro-name='jira']/@src");
-        assertTrue(attributeValue
-                .contains("/confluence/download/resources/confluence.extra.jira/jira-table.png"));
+        String imgSrcAttValueOfMacro = getImageSourceOfMacroElement("xpath=//img[@class='editor-inline-macro' and @data-macro-name='jira']");
+        assertTrue(imgSrcAttValueOfMacro.contains("/confluence/download/resources/confluence.extra.jira/jira-table.png"));
         client.selectFrame("relative=top");
 
     }
