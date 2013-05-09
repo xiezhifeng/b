2778c3231814919db5406802158f59d47cfcc772
khtran <khtran@atlassion.com>
2013-05-09 16:37:09 +0700

    Change tab to space

--- src/main/resources/templates/extra/jira/common.js
+++ src/main/resources/templates/extra/jira/common.js
@@ -1,5 +1,5 @@
 AJS.JiraIssues = {
-		Remote: {}
+        Remote: {}
 };
 var appLinksI18n = {entries:{}};
 jQuery(document).ready(function () {
@@ -45,17 +45,17 @@ jQuery(document).ready(function () {
     });
 
     var loadIssuesForAppId = function(appId){
-    	var issues = AJS.JiraIssues.Remote[appId];
+        var issues = AJS.JiraIssues.Remote[appId];
         var keyClause = '';
         for (var i = 0; i < issues.length; i++){
-        	keyClause = keyClause + (issues[i].key + (i < issues.length - 1 ? ',' : ''));
+            keyClause = keyClause + (issues[i].key + (i < issues.length - 1 ? ',' : ''));
         }
 
         var createQueryUrl = function(keyClause){
             var jqlQuery = 'issuekey in (' + keyClause + ')';
             var jiraUrl = '/sr/jira.issueviews:searchrequest-xml/temp/SearchRequest.xml?jqlQuery=' + encodeURIComponent(jqlQuery);
             var localUrl = contextPath + '/plugins/servlet/issue-retriever?appId=' + appId + '&url=' + encodeURIComponent(jiraUrl) +
-            	'&columns=summary&columns=type&columns=resolution&columns=status';
+                '&columns=summary&columns=type&columns=resolution&columns=status';
             return localUrl;
         };
 
@@ -98,15 +98,15 @@ jQuery(document).ready(function () {
                     var oauthRealm = AJS.JiraIssues.getOAuthRealm(xhr);
 
                     if (oauthRealm){
-                    	var seen = {};
+                        var seen = {};
                         AJS.$(issues).each(function(){
-                        	if (!seen[this.key]){
-                        		seen[this.key] = true;
-	                            var oauthLink = AJS.$('<span class="oauth-msg"> - <a class="oauth-init" href="' + oauthRealm + '">' +
-	                                    AJS.I18n.getText("jiraissues.oauth.linktext") +
-	                                    '</a> ' + AJS.I18n.getText("jiraissues.oauth.single.message") + '</span>');
-	                            AJS.$('.unknown-jira-issue.' + this.key).addClass('single-issue-oauth').append(oauthLink);
-                        	}
+                            if (!seen[this.key]){
+                                seen[this.key] = true;
+                                var oauthLink = AJS.$('<span class="oauth-msg"> - <a class="oauth-init" href="' + oauthRealm + '">' +
+                                        AJS.I18n.getText("jiraissues.oauth.linktext") +
+                                        '</a> ' + AJS.I18n.getText("jiraissues.oauth.single.message") + '</span>');
+                                AJS.$('.unknown-jira-issue.' + this.key).addClass('single-issue-oauth').append(oauthLink);
+                            }
                         });
                         AJS.JiraIssues.bindOAuthLink(AJS.$('.single-issue-oauth a.oauth-init'), function(){
                             window.location.reload();
@@ -117,17 +117,17 @@ jQuery(document).ready(function () {
                     // jira (proxied via conf) can return this if one of the issue keys is invalid.
                     // in that case, we make a request for each key
                     AJS.$(issues).each(function(){
-                    	var key = this.key;
-                    	var keyUrl = createQueryUrl(key);
+                        var key = this.key;
+                        var keyUrl = createQueryUrl(key);
                         AJS.$.ajax({
-                        	url: keyUrl,
-                        	success: attachIssueData,
-                        	error: function(xhr){
-                        	   var linkLocation = AJS.$('.unknown-jira-issue.' + key);
-                        	   linkLocation.removeClass('single-issue-oauth');
-                        	   AJS.$(".oauth-msg", linkLocation).remove();
-                        	   linkLocation.addClass('jira-error');
-                        	}
+                            url: keyUrl,
+                            success: attachIssueData,
+                            error: function(xhr){
+                               var linkLocation = AJS.$('.unknown-jira-issue.' + key);
+                               linkLocation.removeClass('single-issue-oauth');
+                               AJS.$(".oauth-msg", linkLocation).remove();
+                               linkLocation.addClass('jira-error');
+                            }
                         });
                     });
                 }
