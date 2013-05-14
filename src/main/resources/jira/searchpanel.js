97d8aad527aed932a005e48b0fc066fda2ed80b8
hieu.ta <hta@atlassian.com>
2013-05-14 16:20:16 +0700

    Fix CONFVN-150 : Add learn more link after the error message when query JQL

--- src/main/resources/jira/searchpanel.js
+++ src/main/resources/jira/searchpanel.js
@@ -106,7 +106,7 @@ AJS.Editor.JiraConnector.Panel.Search.prototype = AJS.$.extend(AJS.Editor.JiraCo
                                   fourHundredHandler();
                               } else {
                                   $('div.data-table', container).remove();
-                                  thiz.warningMsg(container, AJS.I18n.getText("insert.jira.issue.search.badrequest"));
+                                  thiz.warningMsg(container, AJS.I18n.getText("insert.jira.issue.search.badrequest", Confluence.Templates.ConfluenceJiraPlugin.linkMore({'link': '#'})));
                               }
                             } else {
                                 $('div.data-table', container).remove();
@@ -151,7 +151,7 @@ AJS.Editor.JiraConnector.Panel.Search.prototype = AJS.$.extend(AJS.Editor.JiraCo
                         else {
                             // show error msg for no JQL - CONFVN-79
                             clearPanel();
-                            thiz.warningMsg(container, AJS.I18n.getText("insert.jira.issue.search.badrequest"));
+                            thiz.warningMsg(container, AJS.I18n.getText("insert.jira.issue.search.badrequest", Confluence.Templates.ConfluenceJiraPlugin.linkMore({'link': '#'})));
                         }
                     }
                     else {
