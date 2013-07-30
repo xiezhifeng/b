2f4b58b87ff2c2cb29aa4e7356d11db436092c1d
hieu.ta <hta@atlassian.com>
2013-07-30 10:43:42 +0700

    CONFDEV-18640: fix code conversion

--- src/main/java/com/atlassian/conflunce/plugins/jiracharts/JiraChartMacro.java
+++ src/main/java/com/atlassian/conflunce/plugins/jiracharts/JiraChartMacro.java
@@ -9,20 +9,22 @@ import com.atlassian.confluence.macro.MacroExecutionException;
 public class JiraChartMacro implements Macro {
 
     @Override
-    public String execute(Map<String, String> arg0, String arg1,
-            ConversionContext arg2) throws MacroExecutionException {
+    public String execute(Map<String, String> arg0, String arg1, ConversionContext arg2) throws MacroExecutionException
+    {
         // TODO Auto-generated method stub
         return null;
     }
 
     @Override
-    public BodyType getBodyType() {
+    public BodyType getBodyType()
+    {
         // TODO Auto-generated method stub
         return BodyType.NONE;
     }
 
     @Override
-    public OutputType getOutputType() {
+    public OutputType getOutputType()
+    {
         // TODO Auto-generated method stub
         return OutputType.BLOCK;
     }
