e770a7b44e5f3642068ffa61efb4354a1190b43d
Edith Tom <etom@atlassian.com>
2013-08-06 13:46:55 +1000

    CONFDEV-18546 Create a remote issue link when a blueprint create event is encountered that contains applinkId and issueKey parameters in its context

---
+++ src/main/java/com/atlassian/confluence/plugins/jira/ConfluenceEventListener.java
@@ -0,0 +1,55 @@
+package com.atlassian.confluence.plugins.jira;
+
+import com.atlassian.confluence.event.events.content.page.PageCreateEvent;
+import com.atlassian.confluence.event.events.content.page.PageUpdateEvent;
+import com.atlassian.confluence.pages.AbstractPage;
+import com.atlassian.confluence.plugins.createcontent.events.BlueprintPageCreateEvent;
+import com.atlassian.event.api.EventListener;
+import com.atlassian.event.api.EventPublisher;
+import java.util.Map;
+import org.springframework.beans.factory.DisposableBean;
+
+public class ConfluenceEventListener implements DisposableBean
+{
+    private final EventPublisher eventPublisher;
+    private final JiraRemoteLinkCreator jiraRemoteLinkCreator;
+
+    public ConfluenceEventListener(EventPublisher eventPublisher, JiraRemoteLinkCreator jiraRemoteLinkCreator)
+    {
+        this.eventPublisher = eventPublisher;
+        this.jiraRemoteLinkCreator = jiraRemoteLinkCreator;
+        eventPublisher.register(this);
+    }
+
+    @EventListener
+    public void createJiraRemoteLinks(PageCreateEvent event)
+    {
+        final AbstractPage page = event.getPage();
+        jiraRemoteLinkCreator.createLinksForEmbeddedMacros(page);
+    }
+
+    @EventListener
+    public void createJiraRemoteLinks(PageUpdateEvent event)
+    {
+        final AbstractPage prevPage = event.getOriginalPage();
+        final AbstractPage page = event.getPage();
+        jiraRemoteLinkCreator.createLinksForEmbeddedMacros(prevPage, page);
+    }
+
+    @EventListener
+    public void createJiraRemoteLinks(BlueprintPageCreateEvent event)
+    {
+        final AbstractPage page = event.getPage();
+        final Map<String, Object> context = event.getContext();
+
+        if (context.containsKey("issueKey") && context.containsKey("applinkId"))
+        {
+            jiraRemoteLinkCreator.createLinkToIssue(page, (String) context.get("applinkId"), (String) context.get("issueKey"));
+        }
+    }
+
+    public void destroy() throws Exception
+    {
+        eventPublisher.unregister(this);
+    }
+}
