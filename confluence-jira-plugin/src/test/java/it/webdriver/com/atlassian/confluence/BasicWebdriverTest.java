cf38a8a68d7500037b63fa8d7324548b26dfdf34
hieu.ta <hta@atlassian.com>
2013-09-03 17:28:31 +0700

    CONFDEV-19236: implement integration test for JIRA CHART MACRO

--- confluence-jira-plugin/src/test/java/it/webdriver/com/atlassian/confluence/BasicWebdriverTest.java
+++ confluence-jira-plugin/src/test/java/it/webdriver/com/atlassian/confluence/BasicWebdriverTest.java
@@ -1,31 +1,20 @@
 package it.webdriver.com.atlassian.confluence;
 
-import it.webdriver.com.atlassian.confluence.pageobjects.JiraChartDialog;
+import it.com.atlassian.confluence.plugins.createcontent.pageobjects.ViewPage;
 
 import org.junit.Assert;
 import org.junit.Test;
 
 import com.atlassian.confluence.it.Page;
 import com.atlassian.confluence.it.User;
-import com.atlassian.confluence.pageobjects.page.content.EditContentPage;
 import com.atlassian.confluence.webdriver.AbstractWebDriverTest;
 
 public class BasicWebdriverTest extends AbstractWebDriverTest
 {
-    /*@Test
+    @Test
     public void basicLogin()
     {
         product.login(User.ADMIN, ViewPage.class, Page.TEST);
         Assert.assertTrue(product.gotoHomePage().getHeader().isAdmin());
-    }
-    */
-    @Test
-    public void openSelectMacroDialog()
-    {
-        EditContentPage editPage = product.loginAndEdit(User.ADMIN, Page.TEST);
-        editPage.openMacroBrowser();
-        JiraChartDialog jiraChartDialog = product.getPageBinder().bind(JiraChartDialog.class);
-        jiraChartDialog.open();
-        Assert.assertTrue("Insert JIRA Chart".equals(jiraChartDialog.getTitleDialog().equals("")));
     }
 }
--- confluence-jira-plugin/src/test/java/it/webdriver/com/atlassian/confluence/pageobjects/JiraChartDialog.java
+++ confluence-jira-plugin/src/test/java/it/webdriver/com/atlassian/confluence/pageobjects/JiraChartDialog.java
@@ -1,43 +1,92 @@
 package it.webdriver.com.atlassian.confluence.pageobjects;
 
+import org.openqa.selenium.By;
+
 import com.atlassian.confluence.pageobjects.component.dialog.Dialog;
+import com.atlassian.confluence.pageobjects.page.content.EditContentPage;
 import com.atlassian.pageobjects.elements.ElementBy;
 import com.atlassian.pageobjects.elements.PageElement;
 
 public class JiraChartDialog extends Dialog
 {
-
-    
-    public JiraChartDialog()
-    {
-        super("macro-browser-dialog");
-    }
-
     @ElementBy(id = "macro-jirachart")
     private PageElement clickToJiraChart;
     
-    @ElementBy(className = "dialog-title")
-    private PageElement titleDialog;
-    
     @ElementBy(id = "jira-chart-inputsearch")
-    private PageElement searchInput;
+    private PageElement jqlSearch;
     
-    @ElementBy(cssSelector = ".jira-chart-search bottom")
-    private PageElement previewButton;
+    @ElementBy(id = "jira-chart-border")
+    private PageElement borderImage;
     
-    @ElementBy(className = "jira-chart-img")
-    private PageElement chartImage;
+    public JiraChartDialog()
+    {
+        super("jira-chart");
+    }
     
     public JiraChartDialog open()
     {
         clickToJiraChart.click();
-        waitUntilVisible();
         return this;
     }
 
     public String getTitleDialog()
     {
-        return titleDialog.getValue();
+        return driver.findElement(By.cssSelector("#jira-chart .dialog-title")).getText();
+    }
+    
+    public JiraChartDialog inputJqlSearch(String val)
+    {
+        jqlSearch.clear().type(val);
+        jqlSearch.javascript().execute("jQuery(arguments[0]).trigger(\"change\")");
+        return this;
+    }
+    
+    public JiraChartDialog pasteJqlSearch(String val)
+    {
+        jqlSearch.type(val);
+        jqlSearch.javascript().execute("jQuery(arguments[0]).trigger(\"paste\")");
+        return this;
+    }
+    
+    public String getJqlSearch()
+    {
+        return jqlSearch.getValue();
+    }
+
+    public void clickPreviewButton()
+    {
+        driver.findElement(By.cssSelector("#jira-chart .jira-chart-search button")).click();
+    }
+    
+    public PageElement getPageEleJQLSearch()
+    {
+        return jqlSearch;
+    }
+    
+    public void clickBorderImage()
+    {
+        borderImage.click();
+    }
+    
+    public boolean hadImageInDialog()
+    {
+        return driver.findElement(By.cssSelector("#jira-chart .jira-chart-img .chart-img")).isDisplayed();
+    }
+    
+    public boolean hadBorderImageInDialog()
+    {
+        return driver.findElement(By.cssSelector("#jira-chart .jira-chart-img .chart-img .jirachart-border")).isDisplayed();
+    }
+    
+    public EditContentPage clickInsertDialog()
+    {
+        clickButton("insert-jira-chart-macro-button", false);
+        return pageBinder.bind(EditContentPage.class);
+    }
+    
+    public String getLinkMoreToCome()
+    {
+        return driver.findElement(By.cssSelector("#jira-chart .dialog-page-menu .moreToCome a")).getAttribute("href");
     }
 
 }
