b83a3b373c0f95e87b8a01df7b567367de008698
kpham <kpham@atlassian.com>
2013-09-12 17:54:46 +0700

    CONFDEV-19019: add waiting for create issue

--- confluence-jira-plugin/src/test/java/it/webdriver/com/atlassian/confluence/JiraIssuesMacroWebDriverTest.java
+++ confluence-jira-plugin/src/test/java/it/webdriver/com/atlassian/confluence/JiraIssuesMacroWebDriverTest.java
@@ -5,6 +5,7 @@ import com.atlassian.confluence.it.User;
 import com.atlassian.confluence.pageobjects.component.editor.EditorContent;
 import com.atlassian.confluence.pageobjects.component.editor.MacroPlaceholder;
 import com.atlassian.confluence.pageobjects.page.content.EditContentPage;
+import com.atlassian.pageobjects.elements.query.Poller;
 import it.webdriver.com.atlassian.confluence.pageobjects.JiraMacroDialog;
 import org.junit.Assert;
 import org.junit.Test;
@@ -23,7 +24,7 @@ public class JiraIssuesMacroWebDriverTest extends AbstractJiraWebDriverTest
     }
 
     @Test
-    public void testCreateEpicIssue()
+    public void testCreateEpicIssue() throws InterruptedException
     {
         JiraMacroDialog jiraMacroDialog = openSelectMacroDialog();
         jiraMacroDialog.selectMenuItem("Create New Issue");
@@ -32,7 +33,7 @@ public class JiraIssuesMacroWebDriverTest extends AbstractJiraWebDriverTest
         jiraMacroDialog.setSummary("SUMMARY");
         jiraMacroDialog.setEpicName("TEST EPIC");
         EditorContent editorContent = jiraMacroDialog.insertIssue().getContent();
-
+        wait(5000);
         List<MacroPlaceholder> listMacroChart = editorContent.macroPlaceholderFor("jira");
         Assert.assertEquals(1, listMacroChart.size());
         String htmlMacro = editorContent.getHtml();
