51d80719719c3d2b4080a291bc382a2dd20408c0
duyluong <dluong@atlassian.com>
2013-09-16 11:33:01 +0700

    fixed pull request comments from Hieu

--- confluence-jira-plugin/src/main/java/com/atlassian/confluence/extra/jira/util/JiraUtil.java
+++ confluence-jira-plugin/src/main/java/com/atlassian/confluence/extra/jira/util/JiraUtil.java
@@ -1,8 +1,6 @@
 package com.atlassian.confluence.extra.jira.util;
 
 import java.io.IOException;
-import java.io.UnsupportedEncodingException;
-import java.net.URLEncoder;
 
 import javax.servlet.http.HttpServletResponse;
 
@@ -12,11 +10,6 @@ import org.codehaus.jackson.JsonParseException;
 import org.codehaus.jackson.map.JsonMappingException;
 import org.codehaus.jackson.map.ObjectMapper;
 
-import com.atlassian.applinks.api.ApplicationLink;
-import com.atlassian.applinks.api.ApplicationLinkRequest;
-import com.atlassian.applinks.api.ApplicationLinkRequestFactory;
-import com.atlassian.applinks.api.CredentialsRequiredException;
-import com.atlassian.applinks.api.auth.Anonymous;
 import com.atlassian.confluence.extra.jira.JiraChannelResponseHandler;
 import com.atlassian.confluence.extra.jira.JiraResponseHandler;
 import com.atlassian.confluence.extra.jira.JiraResponseHandler.HandlerType;
@@ -24,12 +17,9 @@ import com.atlassian.confluence.extra.jira.JiraStringResponseHandler;
 import com.atlassian.confluence.extra.jira.exception.AuthenticationException;
 import com.atlassian.confluence.extra.jira.exception.MalformedRequestException;
 import com.atlassian.confluence.json.json.JsonObject;
-import com.atlassian.confluence.json.parser.JSONException;
 import com.atlassian.confluence.json.parser.JSONObject;
 import com.atlassian.confluence.plugins.jira.beans.BasicJiraIssueBean;
 import com.atlassian.confluence.plugins.jira.beans.JiraIssueBean;
-import com.atlassian.sal.api.net.Request;
-import com.atlassian.sal.api.net.ResponseException;
 
 public class JiraUtil
 {
@@ -50,16 +40,19 @@ public class JiraUtil
             if (status == HttpServletResponse.SC_FORBIDDEN)
             {
                 throw new IllegalArgumentException(statusMessage);
-            } else if (status == HttpServletResponse.SC_UNAUTHORIZED)
+            }
+            else if (status == HttpServletResponse.SC_UNAUTHORIZED)
             {
                 throw new AuthenticationException(statusMessage);
-            } else if (status == HttpServletResponse.SC_BAD_REQUEST)
+            }
+            else if (status == HttpServletResponse.SC_BAD_REQUEST)
             {
                 throw new MalformedRequestException(statusMessage);
-            } else
+            }
+            else
             {
-                log.error("Received HTTP " + status + " from server. Error message: "
-                        + StringUtils.defaultString(statusMessage, "No status message"));
+                log.error("Received HTTP " + status + " from server. Error message: " +
+                        StringUtils.defaultString(statusMessage, "No status message"));
                 // we're not sure how to handle any other error conditions at
                 // this point
                 throw new RuntimeException(statusMessage);
@@ -72,19 +65,22 @@ public class JiraUtil
         if (handlerType == HandlerType.CHANNEL_HANDLER)
         {
             return new JiraChannelResponseHandler(url);
-        } else if (handlerType == HandlerType.STRING_HANDLER)
+        }
+        else if (handlerType == HandlerType.STRING_HANDLER)
         {
             return new JiraStringResponseHandler();
-        } else
+        }
+        else
         {
             throw new IllegalStateException("unable to handle " + handlerType);
         }
     }
-    
+
     /**
      * Create JSON string for call JIRA create issue rest api
      * 
-     * @param jiraIssueBean Jira issue inputted
+     * @param jiraIssueBean
+     *            Jira issue inputted
      * @return json string
      */
     public static String createJsonStringForJiraIssueBean(JiraIssueBean jiraIssueBean)
@@ -113,7 +109,7 @@ public class JiraUtil
         basicJiraIssueBean = mapper.readValue(jiraIssueResponseString, BasicJiraIssueBean.class);
         return basicJiraIssueBean;
     }
-    
+
     /**
      * Update jira issue bean fields from basic jira issue bean
      * 
@@ -126,63 +122,10 @@ public class JiraUtil
         jiraIssueBean.setKey(basicJiraIssueBean.getKey());
         jiraIssueBean.setSelf(basicJiraIssueBean.getSelf());
     }
-    
-    public static int getTotalIssue(ApplicationLink appLink, String jql) throws Exception
-    {
-        String result = null;
-        String url = appLink.getRpcUrl() + String.format(TOTAL_ISSUE_FOLLOW_JQL, URLEncoder.encode(jql, "UTF-8") );
-        try
-        {
-            result = requestJiraByAuthenticatedUser(appLink, url);
-        }
-        catch(CredentialsRequiredException e)
-        {
-            result = requestJiraByAnonymousUser(appLink, url);
-        }
-        
-        return getTotalFromStringJSON(result);
-    }
-    
-    /**
-     * Request jira by login user
-     * @param appLink jira server app link
-     * @param url rest url
-     * @return response
-     * @throws CredentialsRequiredException when user is not mapping
-     * @throws ResponseException when have problem request jira server
-     */
-    private static String requestJiraByAuthenticatedUser(ApplicationLink appLink, String url)
-            throws CredentialsRequiredException, ResponseException
-    {
-            final ApplicationLinkRequestFactory requestFactory = appLink.createAuthenticatedRequestFactory();
-            ApplicationLinkRequest request = requestFactory.createRequest(Request.MethodType.GET, url);
-            return request.execute();
-    }
-    
-    /**
-     * Request jira by anonymous user
-     * @param appLink jira server app link
-     * @param url rest url
-     * @return response
-     */
-    private static String requestJiraByAnonymousUser(ApplicationLink appLink, String url)
-    {
-        try
-        {
-            final ApplicationLinkRequestFactory requestFactory = appLink.createAuthenticatedRequestFactory(Anonymous.class);
-            ApplicationLinkRequest request = requestFactory.createRequest(Request.MethodType.GET, url);
-            return request.execute();
-        }
-        catch (Exception e)
-        {
-            log.info("Can not retrieve data from jira server by anonymous user", e);
-            return null;
-        }
-    }
-    
+
     private static int getTotalFromStringJSON(String str) throws Exception
     {
-            JSONObject jsonObject = new JSONObject(str);
-            return jsonObject.getInt("total");
+        JSONObject jsonObject = new JSONObject(str);
+        return jsonObject.getInt("total");
     }
 }
--- confluence-jira-plugin/src/main/java/com/atlassian/confluence/plugins/jira/JiraConnectorStateResource.java
+++ confluence-jira-plugin/src/main/java/com/atlassian/confluence/plugins/jira/JiraConnectorStateResource.java
@@ -1,54 +1,44 @@
 package com.atlassian.confluence.plugins.jira;
 
-import java.net.URLEncoder;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
-import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 
-import org.apache.commons.httpclient.HttpStatus;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import com.atlassian.applinks.api.ApplicationId;
 import com.atlassian.applinks.api.ApplicationLink;
-import com.atlassian.applinks.api.ApplicationLinkRequest;
-import com.atlassian.applinks.api.ApplicationLinkRequestFactory;
 import com.atlassian.applinks.api.ApplicationLinkService;
 import com.atlassian.applinks.api.CredentialsRequiredException;
-import com.atlassian.applinks.api.auth.Anonymous;
-import com.atlassian.confluence.extra.jira.util.JiraUtil;
-import com.atlassian.confluence.json.parser.JSONObject;
 import com.atlassian.plugins.rest.common.security.AnonymousAllowed;
-import com.atlassian.sal.api.net.Request;
 import com.atlassian.sal.api.net.Request.MethodType;
-import com.atlassian.sal.api.net.ResponseException;
 
 @Path("servers")
-@Produces({MediaType.APPLICATION_JSON})
+@Produces({ MediaType.APPLICATION_JSON })
 @AnonymousAllowed
 public class JiraConnectorStateResource
 {
     private static final Logger log = LoggerFactory.getLogger(JiraConnectorStateResource.class);
     private static final String TOTAL_ISSUE_FOLLOW_JQL = "/rest/api/2/search?jql=%s&maxResults=0";
-    
+
     private ApplicationLinkService appLinkService;
 
     public JiraConnectorStateResource(ApplicationLinkService appLinkService)
     {
         this.appLinkService = appLinkService;
     }
-  
+
     @GET
     public Response getJiraServers()
     {
-        Iterable<ApplicationLink> appLinks = appLinkService.getApplicationLinks(com.atlassian.applinks.api.application.jira.JiraApplicationType.class);
+        Iterable<ApplicationLink> appLinks = appLinkService
+                .getApplicationLinks(com.atlassian.applinks.api.application.jira.JiraApplicationType.class);
         if (appLinks != null)
         {
             List<JiraServerBean> servers = new ArrayList<JiraServerBean>();
@@ -59,12 +49,13 @@ public class JiraConnectorStateResource
                 {
                     link.createAuthenticatedRequestFactory().createRequest(MethodType.GET, "");
                 }
-                catch(CredentialsRequiredException e)
+                catch (CredentialsRequiredException e)
                 {
-                    // if an exception is thrown, we need to prompt for oauth                
+                    // if an exception is thrown, we need to prompt for oauth
                     authUrl = e.getAuthorisationURI().toString();
                 }
-                servers.add(new JiraServerBean(link.getId().toString(), link.getRpcUrl().toString(),link.getName(), link.isPrimary(), authUrl));
+                servers.add(new JiraServerBean(link.getId().toString(), link.getRpcUrl().toString(), link.getName(),
+                        link.isPrimary(), authUrl));
             }
             if (!servers.isEmpty())
             {
@@ -72,23 +63,5 @@ public class JiraConnectorStateResource
             }
         }
         return Response.ok(Collections.EMPTY_LIST).build();
-    }
-    
-    @GET
-    @Path("/applink/{appLinkId}/jql/{jql}/totalissue")
-    public Response getTotalIssue(@PathParam("appLinkId") String appLinkId, @PathParam("jql") String jql)
-    {
-       ApplicationLink appLink = null;
-       int total = 0;
-       try
-       {
-            appLink = appLinkService.getApplicationLink(new ApplicationId(appLinkId));
-            total = JiraUtil.getTotalIssue(appLink, jql);
-       } catch(Exception e)
-       {
-           log.error("error get total issue", e);
-           return Response.status(HttpStatus.SC_BAD_REQUEST).build();
-       }
-       return Response.ok(total > 1 ? total + " issues" : total + " issue").build();
     }
 }
