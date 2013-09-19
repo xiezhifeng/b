7e3256f2becf174f3a0904228a63ad98cd25e495
duyluong <dluong@atlassian.com>
2013-09-19 16:22:21 +0700

    CONFDEV-18729 - fixed comments during QA session from Hai

---
+++ confluence-jira-plugin/src/main/java/com/atlassian/confluence/plugins/jiracharts/JiraStatType.java
@@ -0,0 +1,12 @@
+package com.atlassian.confluence.plugins.jiracharts;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: duy.luong
+ * Date: 9/19/13
+ * Time: 3:48 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public enum JiraStatType
+{
+}
