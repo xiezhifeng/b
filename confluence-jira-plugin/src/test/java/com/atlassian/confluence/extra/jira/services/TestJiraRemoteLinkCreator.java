dc25fb81fffa74116c0c8c3a94beccaf34b76330
duyluong <dluong@atlassian.com>
2013-09-26 17:25:55 +0700

    CONF-30891 - fixed, will return the primary app link if there is no information about server id

---
+++ confluence-jira-plugin/src/test/java/com/atlassian/confluence/extra/jira/services/TestJiraRemoteLinkCreator.java
@@ -0,0 +1,12 @@
+package com.atlassian.confluence.extra.jira.services;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: duy.luong
+ * Date: 9/26/13
+ * Time: 4:25 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class TestJiraRemoteLinkCreator
+{
+}
