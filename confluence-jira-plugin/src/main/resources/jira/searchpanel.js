a329e188b005726a322001619e81a5965bf9fc29
duyluong <dluong@atlassian.com>
2013-10-06 22:07:28 +0700

    CONF-30116 - fixed issue reload macro not disable maximunIssues txt box when display option is table

--- confluence-jira-plugin/src/main/resources/jira/searchpanel.js
+++ confluence-jira-plugin/src/main/resources/jira/searchpanel.js
@@ -502,6 +502,8 @@ AJS.Editor.JiraConnector.Panel.Search.prototype = AJS.$.extend(AJS.Editor.JiraCo
             } else {
                 AJS.$("#opt-table").prop("checked", true);
                 // CONF-30116
+
+                AJS.$('#jira-maximum-issues').removeAttr('disabled');
                 var maximumIssues = macroParams["maximumIssues"] || this.DEFAULT_MAX_ISSUES_VAL;
                 this.checkAndSetDefaultValueMaximumIssues({defaultVal : maximumIssues});
             }
