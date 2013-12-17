1b228aa647c9f46f2e0495d66f199e21dacb5845
tdang <tdang@atlassian.com>
2013-12-17 10:58:51 +0700

    CONFDEV-21149: fix integration test for component and create issues

--- confluence-jira-plugin-selenium/src/test/java/it/com/atlassian/confluence/plugins/jira/selenium/CreateIssueTestCase.java
+++ confluence-jira-plugin-selenium/src/test/java/it/com/atlassian/confluence/plugins/jira/selenium/CreateIssueTestCase.java
@@ -35,22 +35,20 @@ public class CreateIssueTestCase extends AbstractJiraPanelTestCase
 
     }
 
-    public void testComponentsVersions()
+    public void testComponents()
     {
         openJiraDialog();
         client.click("//button[text()='Create New Issue']");
         client.waitForAjaxWithJquery();
 
         client.selectFrame("relative=top");
-        client.select("css=select.project-select","index=2");
+        client.select("css=select.project-select","index=4");
 
         client.waitForAjaxWithJquery();
-
-        assertThat.elementPresentByTimeout("//option[contains(text(),'Foo')]");
-        assertThat.elementPresentByTimeout("//option[contains(text(),'Bar')]");
-
-        assertThat.elementPresentByTimeout("//option[contains(text(),'1.0')]");
-        assertThat.elementPresentByTimeout("//option[contains(text(),'2.0')]");
+        final String componentId = "id=components";
+        assertThat.elementPresentByTimeout(componentId);
+        client.select(componentId,"index=0");
+        assertThat.elementContainsText(componentId, "test-component");
     }
 
     public void testCreateIssue()
@@ -64,10 +62,8 @@ public class CreateIssueTestCase extends AbstractJiraPanelTestCase
 
         client.waitForAjaxWithJquery();
 
-        client.select("css=#components", "index=1");
-        client.select("css=#versions", "index=1");
-
         client.typeKeys("css=input.issue-summary", "blah");
+        client.typeKeys("css=input[name='reporter']", "admin");
         assertThat.elementPresent("css=button.insert-issue-button:enabled");
         client.click("css=button.insert-issue-button");
 
@@ -78,7 +74,7 @@ public class CreateIssueTestCase extends AbstractJiraPanelTestCase
 
         //look for JIRA issue in RTE
         client.selectFrame("wysiwygTextarea_ifr");
-        assertThat.elementVisible("//img[@class='editor-inline-macro' and @data-macro-name='jira']");
+        assertThat.elementPresentByTimeout("//img[@class='editor-inline-macro' and @data-macro-name='jira']");
 
         client.selectFrame("relative=top");
     }
