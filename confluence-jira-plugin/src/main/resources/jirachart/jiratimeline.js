1deb5545e9cfe12fe282706c649dfdade6dfc2e2
Phong Nguyen Hong <phnguyen@atlassian.com>
2014-02-26 14:32:21 +0700

    - Before Drop

--- confluence-jira-plugin/src/main/resources/jirachart/jiratimeline.js
+++ confluence-jira-plugin/src/main/resources/jirachart/jiratimeline.js
@@ -2,53 +2,102 @@ var JiraTimeline = window.JiraTimeline || {};
 JiraTimeline = (function($, _) {
     function JiraTimeline(opts) {
         this.timelineClass = '.jira-timeline';
-        this.configuration = _.extend({}, {
-
-        }, opts);
+        this.setupData();
 
         this.init();
     }
 
     JiraTimeline.prototype = {
         init: function() {
-            this.placeHolder = $(this.timelineClass);
+            if (this.$timelineEl.length) {
+                this.loadTimeline();
+                this.setupPackingLot();
+            }
+        },
+        loadTimeline: function() {
+            var googleData = new google.visualization.DataTable();
+            googleData.addColumn('datetime', 'start');
+            googleData.addColumn('datetime', 'end');
+            googleData.addColumn('string', 'content');
+            googleData.addColumn('string', 'group');
 
-            if (this.placeHolder.length) {
-                var data = new google.visualization.DataTable();
-                data.addColumn('datetime', 'start');
-                data.addColumn('datetime', 'end');
-                data.addColumn('string', 'content');
+            googleData.addRows(this.getData().data);
 
-                /*data.addRows([
-                    [new Date(2010,7,23), , 'Conversation<br>' + '<img src="img/comments-icon.png" style="width:32px; height:32px;">'],
-                    [new Date(2010,7,23,23,0,0), , 'Mail from boss<br>' + '<img src="img/mail-icon.png" style="width:32px; height:32px;">'],
-                    [new Date(2010,7,24,16,0,0), , 'Report'],
-                    [new Date(2010,7,26), new Date(2010,8,2), 'Traject A'],
-                    [new Date(2010,7,28), , 'Memo<br>' + '<img src="img/notes-edit-icon.png" style="width:48px; height:48px;">'],
-                    [new Date(2010,7,29), , 'Phone call<br>' + '<img src="img/Hardware-Mobile-Phone-icon.png" style="width:32px; height:32px;">'],
-                    [new Date(2010,7,31), new Date(2010,8,3), 'Traject B'],
-                    [new Date(2010,8,4,12,0,0), , 'Report<br>' + '<img src="img/attachment-icon.png" style="width:32px; height:32px;">']
-                ]);*/
-                data.addRows(this.getData().data);
+            // Instantiate our timeline object.
+            this.timelineObj = new links.Timeline(this.$timelineEl[0]);
 
-                // specify options
-                var options = {
-                    "width":  "100%",
-                    "height": "300px",
-                    "style": "box"
+            // Draw our timeline with the created data and options
+            this.timelineObj.draw(googleData, {
+                width:  this.dataSource.options.width,
+                height: this.dataSource.options.height,
+                style: this.dataSource.options.style,
+
+                groupsChangeable : true,
+                groupsOnRight: false,
+                //groupsChangeable: true,
+                groupsOrder: true,
+
+                editable: true,
+                showCustomTime: true
+            });
+        },
+        setupData: function() {
+            this.$timelineEl = $(this.timelineClass);
+            var attr = this.$timelineEl.attr('datasource');
+            this.dataSource = window[attr];
+        },
+        getData: function() {
+            if (!this.issueList) {
+                var data = [], packing = [];
+                _.each(this.dataSource.data, function(item) {
+                    if (item[0] === undefined) {
+                        packing.push(item);
+                    } else {
+                        data.push(item);
+                    }
+                });
+                this.issueList = {
+                    data: data,
+                    packing: packing
                 };
+            }
+            return this.issueList;
+        },
+        setupPackingLot: function() {
+            this.$packing = $('<div />', {
+                'class': 'jira-timeline-packinglot'
+            });
+            this.$timelineEl.after(this.$packing);
 
-                // Instantiate our timeline object.
-                timeline = new links.Timeline(this.placeHolder[0]);
-
-                // Draw our timeline with the created data and options
-                timeline.draw(data, options);
+            if (this.issueList.packing.length) {
+                this.loadPackingLot();
 
             }
         },
-        getData: function() {
-            var attr = this.placeHolder.attr('datasource');
-            return window[attr];
+        loadPackingLot: function() {
+            var item = Confluence.Templates.Timeline.packingItem({ list: this.issueList.packing});
+            this.$packing.append(item);
+
+            this.setupDragDrop();
+        },
+        setupDragDrop: function() {
+            $('.packing-item', this.$packing).draggable({
+                cursor: 'move',
+                revert: true,
+                helper: 'clone'
+            });
+
+            $('.timeline-groups-text', this.$timelineEl).droppable({
+                accept: '.packing-item',
+                activeClass: 'timeline-groups-text-active',
+                hoverClass: 'timeline-groups-text-hover',
+                drop: _.bind(this.onDrop, this)
+            });
+        },
+        onDrop: function(e, ui) {
+            var customTime = this.timelineObj.getCustomTime();
+            var itemDrag = ui.draggable[0].innerHTML;
+            //this.timelineObj.addItem([customTime, undefined, itemDrag, e.target.innerHTML]);
         }
     };
 
