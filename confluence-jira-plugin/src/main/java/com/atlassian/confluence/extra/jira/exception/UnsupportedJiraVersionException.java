c5f98eb325f3d657635237bb24b38d5fcbd64606
Tam Tran Minh <ttranminh@atlassian.com>
2014-03-21 17:44:30 +0700

    remove unnecessary class

--- confluence-jira-plugin/src/main/java/com/atlassian/confluence/extra/jira/exception/UnsupportedJiraVersionException.java
+++
@@ -1,9 +0,0 @@
-package com.atlassian.confluence.extra.jira.exception;
-
-public class UnsupportedJiraVersionException extends Exception
-{
-    public UnsupportedJiraVersionException()
-    {
-        super();
-    }
-}
