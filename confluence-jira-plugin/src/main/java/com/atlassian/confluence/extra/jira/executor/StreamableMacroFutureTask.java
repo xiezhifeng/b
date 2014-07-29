c7fb8e9dc748a6faaecf8cf530859a5b16bab682
Tam Tran Minh <ttranminh@atlassian.com>
2014-07-29 17:40:53 +0700

    performance improvement for single issues entered in wiki format {jira:ABC-xyz}

--- confluence-jira-plugin/src/main/java/com/atlassian/confluence/extra/jira/executor/StreamableMacroFutureTask.java
+++ confluence-jira-plugin/src/main/java/com/atlassian/confluence/extra/jira/executor/StreamableMacroFutureTask.java
@@ -7,6 +7,7 @@ import com.atlassian.confluence.content.render.xhtml.ConversionContext;
 import com.atlassian.confluence.extra.jira.JiraIssuesMacro;
 import com.atlassian.confluence.extra.jira.exception.UnsupportedJiraServerException;
 import com.atlassian.confluence.extra.jira.helper.JiraExceptionHelper;
+import com.atlassian.confluence.extra.jira.util.JiraUtil;
 import com.atlassian.confluence.macro.StreamableMacro;
 import com.atlassian.confluence.user.AuthenticatedUserThreadLocal;
 import com.atlassian.confluence.user.ConfluenceUser;
@@ -61,7 +62,8 @@ public class StreamableMacroFutureTask implements Callable<String>
             if (element != null) // is single issue jira markup and in batch
             {
                 final JiraIssuesMacro jiraIssuesMacro = (JiraIssuesMacro) macro;
-                final String key = parameters.get(JiraIssuesMacro.KEY);
+                final String key = JiraUtil.getSingleIssueKey(parameters, false);
+                // tryPatternMatching = false because we already know it's a single issue
                 return jiraIssuesMacro.renderSingleJiraIssue(parameters, context, element, jiraServerUrl, key);
             }
             else if (exception != null)
