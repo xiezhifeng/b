d60033d696dbbfaa41e265a0d66e2c982f0bf072
Khoa Pham <kpham@atlassian.com>
2014-08-07 14:51:12 +0700

    Merge branch 'master' into issue/CONF-34264

--- confluence-jira-plugin/src/main/java/com/atlassian/confluence/plugins/jira/ChartProxyServlet.java
+++ confluence-jira-plugin/src/main/java/com/atlassian/confluence/plugins/jira/ChartProxyServlet.java
@@ -25,7 +25,6 @@ import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.net.URI;
 import java.util.Map;
-import java.util.Map;
 
 public class ChartProxyServlet extends AbstractProxyServlet
 {
