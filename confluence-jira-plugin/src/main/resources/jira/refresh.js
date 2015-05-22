486ffd943a437c7e1ef21fe7253af78709432332
Tung Dang <tthanhdang@atlassian.com>
2015-05-22 14:14:25 +0700

    CONFDEV-33795 - update test message for refresh JIM table case

--- confluence-jira-plugin/src/main/resources/jira/refresh.js
+++ confluence-jira-plugin/src/main/resources/jira/refresh.js
@@ -124,14 +124,7 @@ RefreshMacro.CallbackSupport = function(refresh) {
 RefreshMacro.CallbackSupport.prototype = {
     errorHandler: function(err) {
         var widget = RefreshWidget.get(this.refresh.id);
-
-        var $err = $(err);
-        // just get error message only, do not get entire message because it contains a table markup.
-        if ($err.hasClass('jim-error-message-table')) {
-            err = $('<div></div>').append($err.find('.jira-link')).html();
-        }
-
-        var errMsg = AJS.I18n.getText('jiraissues.error.refresh') + err;
+        var errMsg = AJS.I18n.getText('jiraissues.error.refresh');
         widget.getErrorMessagePanel().html(errMsg);
         widget.updateRefreshVisibility(RefreshMacro.REFRESH_STATE_FAILED);
     },
