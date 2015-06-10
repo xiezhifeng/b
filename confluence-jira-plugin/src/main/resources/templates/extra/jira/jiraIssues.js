314bac76cae4290a49d9cd709c1e9e27673ec938
Tung Dang <tthanhdang@atlassian.com>
2015-06-10 14:52:20 +0700

    CONFDEV-34308: Support warning-icon in error message on old confluence

--- confluence-jira-plugin/src/main/resources/templates/extra/jira/jiraIssues.js
+++ confluence-jira-plugin/src/main/resources/templates/extra/jira/jiraIssues.js
@@ -292,11 +292,57 @@ jQuery(document).ready(function () {
             }
 
             return columnWidths;
+        },
+
+        fixBreakIconInOldConf: function() {
+            // CONF with AUI <= 5.4.4 does not support icon font in warning message box.
+            var isCONFNotSupportWarningIconFont =  JiraIssues.compareVersion(AJS.version, '5.4.4') <= 0;
+            if (!isCONFNotSupportWarningIconFont) {
+                return;
+            }
+
+            $('.jim-error-message').each(function() {
+                var $this = $(this);
+                var $message = $this;
+
+                if ($this.hasClass('jim-error-message-table')) {
+                    $message = $this.find('.aui-message')
+                }
+
+                $message
+                        .addClass('warning')
+                        .prepend('<span class="aui-icon icon-warning"></span>')
+            });
+        },
+
+        compareVersion: function(left, right) {
+            if (typeof left !== 'string' ||
+                typeof right !== 'string') {
+                return false;
+            }
+
+            var a = left.split('.');
+            var b = right.split('.');
+            var i = 0;
+            var len = Math.max(a.length, b.length);
+
+            for (; i < len; i++) {
+                if ((a[i] && !b[i] && parseInt(a[i]) > 0) ||
+                        (parseInt(a[i]) > parseInt(b[i]))) {
+                    return 1;
+                } else if ((b[i] && !a[i] && parseInt(b[i]) > 0) ||
+                            (parseInt(a[i]) < parseInt(b[i]))) {
+                    return -1;
+                }
+            }
+
+            return 0;
         }
     });
 
 
     JiraIssues.fixMenusShowingUnderWidgetInIE();
+    JiraIssues.fixBreakIconInOldConf();
 
     jQuery(".jiraissues_table").each(function (i, jiraissuesTableDiv) {
         var $jiraissuesTableDiv = jQuery(jiraissuesTableDiv),
@@ -438,5 +484,6 @@ jQuery(document).ready(function () {
             }
         });
     });
+
 
 });
