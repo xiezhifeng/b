1aa7dc9ea1db90d4874e16e217a02317a4cdbc39
Kenny MacLeod <kmacleod@atlassian.com>
2015-06-17 22:28:15 +1000

    CONFDEV-34538 - fully implement the metrics event

--- confluence-jira-plugin/src/main/java/com/atlassian/confluence/extra/jira/metrics/JiraIssuesMacroRenderEvent.java
+++ confluence-jira-plugin/src/main/java/com/atlassian/confluence/extra/jira/metrics/JiraIssuesMacroRenderEvent.java
@@ -1,20 +1,57 @@
 package com.atlassian.confluence.extra.jira.metrics;
 
 import com.atlassian.analytics.api.annotations.EventName;
-import com.atlassian.confluence.extra.jira.JiraIssuesMacro;
+import com.atlassian.confluence.extra.jira.JiraIssuesMacro.JiraIssuesType;
+import com.google.common.base.Stopwatch;
+import org.joda.time.Duration;
+
+import java.util.concurrent.atomic.AtomicLong;
+
+import static java.util.concurrent.TimeUnit.MILLISECONDS;
 
 @EventName("confluence.macro.metrics.jiraissues")
 public class JiraIssuesMacroRenderEvent
 {
     private final boolean staticMode;
     private final boolean isMobile;
-    private final JiraIssuesMacro.JiraIssuesType issuesType;
+    private final JiraIssuesType issuesType;
+    private final Duration appLinkResolutionAccumulator;
+    private final Duration buildTemplateModelAccumulator;
+    private final Duration appLinkRequestAccumulator;
+    private final Duration templateRenderAccumulator;
 
-    public JiraIssuesMacroRenderEvent(final boolean staticMode, final boolean isMobile, final JiraIssuesMacro.JiraIssuesType issuesType)
+    public JiraIssuesMacroRenderEvent(
+            final boolean staticMode, final boolean isMobile, final JiraIssuesType issuesType,
+            final Duration appLinkResolutionAccumulator, final Duration buildTemplateModelAccumulator,
+            final Duration appLinkRequestAccumulator, final Duration templateRenderAccumulator)
     {
         this.staticMode = staticMode;
         this.isMobile = isMobile;
         this.issuesType = issuesType;
+        this.appLinkResolutionAccumulator = appLinkResolutionAccumulator;
+        this.buildTemplateModelAccumulator = buildTemplateModelAccumulator;
+        this.appLinkRequestAccumulator = appLinkRequestAccumulator;
+        this.templateRenderAccumulator = templateRenderAccumulator;
+    }
+
+    public long getAppLinkResolutionAccumulatorMillis()
+    {
+        return appLinkResolutionAccumulator.getMillis();
+    }
+
+    public long getBuildTemplateModelAccumulatorMillis()
+    {
+        return buildTemplateModelAccumulator.getMillis();
+    }
+
+    public long getAppLinkRequestAccumulatorMillis()
+    {
+        return appLinkRequestAccumulator.getMillis();
+    }
+
+    public long getTemplateRenderAccumulatorMillis()
+    {
+        return templateRenderAccumulator.getMillis();
     }
 
     public boolean isStaticMode()
@@ -27,7 +64,7 @@ public class JiraIssuesMacroRenderEvent
         return isMobile;
     }
 
-    public JiraIssuesMacro.JiraIssuesType getIssuesType()
+    public JiraIssuesType getIssuesType()
     {
         return issuesType;
     }
@@ -40,86 +77,70 @@ public class JiraIssuesMacroRenderEvent
     public static class Builder
     {
         private boolean staticMode;
-        private JiraIssuesMacro.JiraIssuesType issuesType;
+        private JiraIssuesType issuesType;
         private boolean isMobile;
+
+        private final AtomicLong appLinkResolutionAccumulator = new AtomicLong();
+        private final AtomicLong buildTemplateModelAccumulator = new AtomicLong();
+        private final AtomicLong appLinkRequestAccumulator = new AtomicLong();
+        private final AtomicLong templateRenderAccumulator = new AtomicLong();
 
         public JiraIssuesMacroRenderEvent build()
         {
-            return new JiraIssuesMacroRenderEvent(staticMode, isMobile, issuesType);
+            return new JiraIssuesMacroRenderEvent(
+                    staticMode, isMobile, issuesType,
+                    duration(appLinkResolutionAccumulator),
+                    duration(buildTemplateModelAccumulator),
+                    duration(appLinkRequestAccumulator),
+                    duration(templateRenderAccumulator)
+            );
+        }
+
+        private static Duration duration(Number accumulator)
+        {
+            return Duration.millis(accumulator.longValue());
         }
 
         public Timer applinkResolutionTimer()
         {
-            return new Timer()
-            {
-                @Override
-                public Timer start()
-                {
-                    return this;
-                }
-
-                @Override
-                public void stop()
-                {
-
-                }
-            };
+            return timer(appLinkResolutionAccumulator);
         }
 
         public Timer buildTemplateModelTimer()
         {
-            return new Timer()
-            {
-                @Override
-                public Timer start()
-                {
-                    return this;
-                }
-
-                @Override
-                public void stop()
-                {
-
-                }
-            };
+            return timer(buildTemplateModelAccumulator);
         }
 
         public Timer appLinkRequestTimer()
         {
-            return new Timer()
-            {
-                @Override
-                public Timer start()
-                {
-                    return this;
-                }
-
-                @Override
-                public void stop()
-                {
-
-                }
-            };
+            return timer(appLinkRequestAccumulator);
         }
 
-        public Timer templateRenderTimer(final boolean staticMode, final JiraIssuesMacro.JiraIssuesType issuesType, final boolean isMobile)
+        public Timer templateRenderTimer(final boolean staticMode, final JiraIssuesType issuesType, final boolean isMobile)
         {
             this.staticMode = staticMode;
             this.issuesType = issuesType;
             this.isMobile = isMobile;
 
+            return timer(templateRenderAccumulator);
+        }
+
+        private static Timer timer(final AtomicLong milliSecondsAccumulator)
+        {
+            final Stopwatch stopwatch = new Stopwatch();
             return new Timer()
             {
                 @Override
                 public Timer start()
                 {
+                    stopwatch.start();
                     return this;
                 }
 
                 @Override
                 public void stop()
                 {
-
+                    milliSecondsAccumulator.addAndGet(stopwatch.elapsedTime(MILLISECONDS));
                 }
             };
         }
---
+++ confluence-jira-plugin/src/main/java/com/atlassian/confluence/extra/jira/metrics/package-info.java
@@ -0,0 +1,4 @@
+@ParametersAreNonnullByDefault
+package com.atlassian.confluence.extra.jira.metrics;
+
+import javax.annotation.ParametersAreNonnullByDefault;
