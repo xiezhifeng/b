78495bd8677114b23e4f0ab3543bc2012bf4845e
Tung Dang <tthanhdang@atlassian.com>
2015-06-26 09:01:36 +0700

    fix flaky test

--- stateless-webdriver-tests/src/test/java/it/com/atlassian/confluence/plugins/webdriver/jiraissues/searchpanel/VerifyOldMacroTest.java
+++ stateless-webdriver-tests/src/test/java/it/com/atlassian/confluence/plugins/webdriver/jiraissues/searchpanel/VerifyOldMacroTest.java
@@ -19,14 +19,14 @@ public class VerifyOldMacroTest extends AbstractJiraIssuesSearchPanelTest
     {
         String jiraMacro = "{jiraissues:" + JIRA_DISPLAY_URL + "/sr/jira.issueviews:searchrequest-xml/temp/SearchRequest.xml?jqlQuery=project+%3D+TP}";
         convertJiraIssuesToJiraMacro(editPage, jiraMacro, "project = TP", OLD_JIRA_ISSUE_MACRO_NAME);
-        assertThat(getMacroParams(editPage, OLD_JIRA_ISSUE_MACRO_NAME), containsString("jqlQuery= project \\= TP"));
+        assertThat(getMacroParams(editPage, JIRA_ISSUE_MACRO_NAME), containsString("jqlQuery= project \\= TP"));
     }
 
     @Test
     public void testConvertJiraIssueToJiraWithKey() {
         String jiraIssuesMacro = "{jiraissues:key=TP-1}";
         convertJiraIssuesToJiraMacro(editPage,jiraIssuesMacro, "key = TP-1", OLD_JIRA_ISSUE_MACRO_NAME);
-        assertThat(getMacroParams(editPage, OLD_JIRA_ISSUE_MACRO_NAME), containsString("key=TP-1"));
+        assertThat(getMacroParams(editPage, JIRA_ISSUE_MACRO_NAME), containsString("key=TP-1"));
     }
 
     @Test
@@ -67,14 +67,14 @@ public class VerifyOldMacroTest extends AbstractJiraIssuesSearchPanelTest
     public void testConvertJiraIssueToJiraWithColumns()
     {
         convertJiraIssuesToJiraMacro(editPage, "{jiraissues:status=open|columns=key,summary,type}", "status = open", OLD_JIRA_ISSUE_MACRO_NAME);
-        assertThat(getMacroParams(editPage, OLD_JIRA_ISSUE_MACRO_NAME), containsString("columns=key,summary,type"));
+        assertThat(getMacroParams(editPage, JIRA_ISSUE_MACRO_NAME), containsString("columns=key,summary,type"));
     }
 
     @Test
     public void testConvertJiraIssueToJiraWithCount()
     {
         convertJiraIssuesToJiraMacro(editPage, "{jiraissues:status=open|count=true}", "status = open", OLD_JIRA_ISSUE_MACRO_NAME);
-        assertThat(getMacroParams(editPage, OLD_JIRA_ISSUE_MACRO_NAME), containsString("count=true"));
+        assertThat(getMacroParams(editPage, JIRA_ISSUE_MACRO_NAME), containsString("count=true"));
     }
 
     @Test
