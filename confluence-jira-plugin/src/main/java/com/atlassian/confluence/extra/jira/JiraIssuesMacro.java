80478ab508b80dcbc56b167d2c22e324e02869b0
tdang <tdang@atlassian.com>
2015-08-06 16:14:00 +0700

    No need to pur clientid in renderSingleIssue

--- confluence-jira-plugin/src/main/java/com/atlassian/confluence/extra/jira/JiraIssuesMacro.java
+++ confluence-jira-plugin/src/main/java/com/atlassian/confluence/extra/jira/JiraIssuesMacro.java
@@ -1093,7 +1093,6 @@ public class JiraIssuesMacro extends BaseMacro implements Macro, EditorImagePlac
     public String renderSingleJiraIssue(Map<String, String> parameters, ConversionContext conversionContext, Element issue, String serverUrl) throws Exception {
         Map<String, Object> contextMap = MacroUtils.defaultVelocityContext();
         String outputType = conversionContext.getOutputType();
-        contextMap.put(CLIENT_ID, parameters.get(CLIENT_ID));
         // added parameters for pdf export
         setRenderMode(contextMap, outputType);
 
