6a22b33ead2651108e9eeb91241ff48aae93ddac
tdang <tdang@atlassian.com>
2015-10-14 10:07:58 +0700

    CONFDEV-36931: code format for JiraSprintModel

--- confluence-jira-plugin/src/main/java/com/atlassian/confluence/plugins/sprint/model/JiraSprintModel.java
+++ confluence-jira-plugin/src/main/java/com/atlassian/confluence/plugins/sprint/model/JiraSprintModel.java
@@ -1,106 +1,61 @@
 package com.atlassian.confluence.plugins.sprint.model;
 
-import java.util.Date;
-
 public class JiraSprintModel
 {
     private String id;
     private String name;
     private String state;
-    private int linkedPagesCount;
-//    private Date startDate;
-//    private Date endDate;
-//    private Date completeDate;
-    private String[] remoteLinks;
-    private int daysRemaining;
     private int originBoardId;
     private String boardUrl;
 
-    public String getId() {
+    public String getId()
+    {
         return id;
     }
 
-    public void setId(String id) {
+    public void setId(String id)
+    {
         this.id = id;
     }
 
-    public String getName() {
+    public String getName()
+    {
         return name;
     }
 
-    public void setName(String name) {
+    public void setName(String name)
+    {
         this.name = name;
     }
 
-    public String getState() {
+    public String getState()
+    {
         return state;
     }
 
-    public void setState(String state) {
+    public void setState(String state)
+    {
         this.state = state;
     }
 
-    public int getLinkedPagesCount() {
-        return linkedPagesCount;
-    }
 
-    public void setLinkedPagesCount(int linkedPagesCount) {
-        this.linkedPagesCount = linkedPagesCount;
-    }
-
-//    public Date getStartDate() {
-//        return startDate;
-//    }
-//
-//    public void setStartDate(Date startDate) {
-//        this.startDate = startDate;
-//    }
-//
-//    public Date getEndDate() {
-//        return endDate;
-//    }
-//
-//    public void setEndDate(Date endDate) {
-//        this.endDate = endDate;
-//    }
-//
-//    public Date getCompleteDate() {
-//        return completeDate;
-//    }
-//
-//    public void setCompleteDate(Date completeDate) {
-//        this.completeDate = completeDate;
-//    }
-
-    public String[] getRemoteLinks() {
-        return remoteLinks;
-    }
-
-    public void setRemoteLinks(String[] remoteLinks) {
-        this.remoteLinks = remoteLinks;
-    }
-
-    public int getDaysRemaining() {
-        return daysRemaining;
-    }
-
-    public void setDaysRemaining(int daysRemaining) {
-        this.daysRemaining = daysRemaining;
-    }
-
-    public void setOriginBoardId(int originBoardId) {
+    public void setOriginBoardId(int originBoardId)
+    {
         this.originBoardId = originBoardId;
     }
 
-    public int getOriginBoardId() {
+    public int getOriginBoardId()
+    {
         return originBoardId;
     }
 
-    public String getBoardUrl() {
+    public String getBoardUrl()
+    {
         return boardUrl;
     }
 
-    public void setBoardUrl(String boardUrl) {
+    public void setBoardUrl(String boardUrl)
+    {
         this.boardUrl = boardUrl;
     }
 }
