eea3f602ac297dd2297f08738f6b7d642751d1e0
tdang <tdang@atlassian.com>
2015-10-22 15:08:04 +0700

    CONFDEV-37493: return `findJiraIssueMacros`, because it needs to be used in confluence-jira-content

--- confluence-jira-plugin/src/main/java/com/atlassian/confluence/extra/jira/api/services/JiraMacroFinderService.java
+++ confluence-jira-plugin/src/main/java/com/atlassian/confluence/extra/jira/api/services/JiraMacroFinderService.java
@@ -2,16 +2,28 @@ package com.atlassian.confluence.extra.jira.api.services;
 
 import com.atlassian.confluence.content.render.xhtml.XhtmlException;
 import com.atlassian.confluence.core.ContentEntityObject;
+import com.atlassian.confluence.pages.AbstractPage;
 import com.atlassian.confluence.xhtml.api.MacroDefinition;
 import com.google.common.base.Predicate;
 
 import java.util.List;
+import java.util.Set;
 
 /**
  * This service responsible for finding all macro definitions in a page or a string
  */
 public interface JiraMacroFinderService
 {
+    /**
+     * Find all JIRA Issue Macros in the page satisfying the search filter
+     *
+     * @param page the page where we want to find the JIRA issues macro
+     * @param filter a custom search filter for refining the results
+     * @return the set of MacroDefinition instances
+     * @throws XhtmlException
+     */
+    Set<MacroDefinition> findJiraIssueMacros(AbstractPage page, Predicate<MacroDefinition> filter) throws XhtmlException;
+
     /**
      * Find all JIRA Macros in the page satisfying the search filter
      *
--- confluence-jira-plugin/src/main/java/com/atlassian/confluence/extra/jira/services/DefaultJiraMacroFinderService.java
+++ confluence-jira-plugin/src/main/java/com/atlassian/confluence/extra/jira/services/DefaultJiraMacroFinderService.java
@@ -5,14 +5,17 @@ import com.atlassian.confluence.content.render.xhtml.XhtmlException;
 import com.atlassian.confluence.core.ContentEntityObject;
 import com.atlassian.confluence.extra.jira.api.services.JiraMacroFinderService;
 import com.atlassian.confluence.extra.jira.util.JiraIssuePredicates;
+import com.atlassian.confluence.pages.AbstractPage;
 import com.atlassian.confluence.xhtml.api.MacroDefinition;
 import com.atlassian.confluence.xhtml.api.MacroDefinitionHandler;
 import com.atlassian.confluence.xhtml.api.XhtmlContent;
 import com.google.common.base.Predicate;
 import com.google.common.base.Predicates;
 import com.google.common.collect.Lists;
+import com.google.common.collect.Sets;
 
 import java.util.List;
+import java.util.Set;
 
 public class DefaultJiraMacroFinderService implements JiraMacroFinderService
 {
@@ -21,6 +24,20 @@ public class DefaultJiraMacroFinderService implements JiraMacroFinderService
     public DefaultJiraMacroFinderService(XhtmlContent xhtmlContent)
     {
         this.xhtmlContent = xhtmlContent;
+    }
+
+    /**
+     * Find all JIRA Issue Macros in the page satisfying the search filter
+     *
+     * @param page   the page where we want to find the JIRA Issues Macros
+     * @param filter the custom search filter for refining the results
+     * @return the set of MacroDefinition instances
+     * @throws XhtmlException
+     */
+    @Override
+    public Set<MacroDefinition> findJiraIssueMacros(AbstractPage page, Predicate<MacroDefinition> filter) throws XhtmlException
+    {
+        return Sets.newHashSet(findJiraMacros(page, JiraIssuePredicates.isJiraIssueMacro));
     }
 
     /**
