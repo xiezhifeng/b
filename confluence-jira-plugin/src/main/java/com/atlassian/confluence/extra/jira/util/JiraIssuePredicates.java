c4af8c29ea3c1f2eee738c5bf6f478710ea1262e
tdang <tdang@atlassian.com>
2015-10-22 10:14:31 +0700

    CONFDEV-37493: refactoring jirafinderservice

--- confluence-jira-plugin/src/main/java/com/atlassian/confluence/extra/jira/util/JiraIssuePredicates.java
+++ confluence-jira-plugin/src/main/java/com/atlassian/confluence/extra/jira/util/JiraIssuePredicates.java
@@ -1,8 +1,10 @@
 package com.atlassian.confluence.extra.jira.util;
 
+import com.atlassian.confluence.extra.jira.JiraIssuesMacro;
 import com.atlassian.confluence.plugins.sprint.JiraSprintMacro;
 import com.atlassian.confluence.xhtml.api.MacroDefinition;
 import com.google.common.base.Predicate;
+import com.google.common.base.Predicates;
 import org.apache.commons.lang.StringUtils;
 
 import java.util.regex.Pattern;
@@ -11,7 +13,17 @@ public class JiraIssuePredicates
 {
     public final static Pattern ISSUE_KEY_PATTERN = Pattern.compile("\\s*([A-Z][A-Z]+)-[0-9]+\\s*");
 
-    public static Predicate<MacroDefinition> isSingleIssue = new Predicate<MacroDefinition>()
+    public static Predicate<MacroDefinition> isJiraIssueMacro = new Predicate<MacroDefinition>()
+    {
+        @Override
+        public boolean apply(MacroDefinition macroDefinition)
+        {
+            return StringUtils.equals(macroDefinition.getName(), JiraIssuesMacro.JIRA)
+                    || StringUtils.equals(macroDefinition.getName(), JiraIssuesMacro.JIRAISSUES);
+        }
+    };
+
+    public static Predicate<MacroDefinition> isSingleIssue = Predicates.and(isJiraIssueMacro, new Predicate<MacroDefinition>()
         {
             @Override
             public boolean apply(MacroDefinition macroDefinition)
@@ -21,14 +33,14 @@ public class JiraIssuePredicates
                     return (defaultParam != null && ISSUE_KEY_PATTERN.matcher(defaultParam).matches()) ||
                            (parameters != null && parameters.get("key") != null);
             }
-        };
+        });
 
     public static Predicate<MacroDefinition> isSprintMacro = new Predicate<MacroDefinition>()
     {
         @Override
         public boolean apply(MacroDefinition macroDefinition)
         {
-            return StringUtils.isNotBlank(macroDefinition.getParameter(JiraSprintMacro.MACRO_ID_PARAMETER));
+            return StringUtils.equals(macroDefinition.getName(), JiraSprintMacro.JIRASPRINT);
         }
     };
 }
