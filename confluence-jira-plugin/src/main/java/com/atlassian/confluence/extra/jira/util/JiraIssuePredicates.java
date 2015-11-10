7861a6f0659146c06c56404b6109a450beb2c90f
tdang <tdang@atlassian.com>
2015-11-10 16:17:38 +0700

    CONFDEV-37985: this code has been refactored on master, so I tried to merge back from there to this branch, so we can utilize JiraMacroFinderService

--- confluence-jira-plugin/src/main/java/com/atlassian/confluence/extra/jira/util/JiraIssuePredicates.java
+++ confluence-jira-plugin/src/main/java/com/atlassian/confluence/extra/jira/util/JiraIssuePredicates.java
@@ -1,7 +1,10 @@
 package com.atlassian.confluence.extra.jira.util;
 
+import com.atlassian.confluence.extra.jira.JiraIssuesMacro;
 import com.atlassian.confluence.xhtml.api.MacroDefinition;
 import com.google.common.base.Predicate;
+import com.google.common.base.Predicates;
+import org.apache.commons.lang.StringUtils;
 
 import java.util.regex.Pattern;
 
@@ -9,15 +12,27 @@ public class JiraIssuePredicates
 {
     public final static Pattern ISSUE_KEY_PATTERN = Pattern.compile("\\s*([A-Z][A-Z]+)-[0-9]+\\s*");
 
-    public static Predicate<MacroDefinition> isSingleIssue = new Predicate<MacroDefinition>()
+    public static Predicate<MacroDefinition> isJiraIssueMacro = new Predicate<MacroDefinition>()
+    {
+        @Override
+        public boolean apply(MacroDefinition macroDefinition)
         {
-            @Override
-            public boolean apply(MacroDefinition macroDefinition)
-            {
-                    String defaultParam = macroDefinition.getDefaultParameterValue();
-                    java.util.Map<String, String> parameters = macroDefinition.getParameters();
-                    return (defaultParam != null && ISSUE_KEY_PATTERN.matcher(defaultParam).matches()) ||
-                           (parameters != null && parameters.get("key") != null);
-            }
-        };
+            return StringUtils.equals(macroDefinition.getName(), JiraIssuesMacro.JIRA)
+                    || StringUtils.equals(macroDefinition.getName(), JiraIssuesMacro.JIRAISSUES);
+        }
+    };
+
+    public static Predicate<MacroDefinition> isSingleIssue = Predicates.and(isJiraIssueMacro, new Predicate<MacroDefinition>()
+    {
+        @Override
+        public boolean apply(MacroDefinition macroDefinition)
+        {
+            String defaultParam = macroDefinition.getDefaultParameterValue();
+            java.util.Map<String, String> parameters = macroDefinition.getParameters();
+            return (defaultParam != null && ISSUE_KEY_PATTERN.matcher(defaultParam).matches()) ||
+                    (parameters != null && parameters.get("key") != null);
+        }
+    });
+
+
 }
