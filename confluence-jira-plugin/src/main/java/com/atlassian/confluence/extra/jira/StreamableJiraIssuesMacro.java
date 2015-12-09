f0bbbb8ca7ad5783ac8532322a0e4af2ba9526d1
tdang <tdang@atlassian.com>
2015-12-09 18:29:45 +0700

    CONFDEV-38859: remove unsed import class

--- confluence-jira-plugin/src/main/java/com/atlassian/confluence/extra/jira/StreamableJiraIssuesMacro.java
+++ confluence-jira-plugin/src/main/java/com/atlassian/confluence/extra/jira/StreamableJiraIssuesMacro.java
@@ -51,9 +51,6 @@ import org.slf4j.LoggerFactory;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
-import java.util.concurrent.Callable;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
 import java.util.concurrent.Future;
 
 /**
