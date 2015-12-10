682aa960ad2561c2ccbac11d4faf5c6ec1d6c1e6
tdang <tdang@atlassian.com>
2015-12-10 10:28:18 +0700

    CONFDEV-38859: add code comment

--- confluence-jira-plugin/src/main/java/com/atlassian/confluence/extra/jira/executor/StreamableMacroFutureTask.java
+++ confluence-jira-plugin/src/main/java/com/atlassian/confluence/extra/jira/executor/StreamableMacroFutureTask.java
@@ -51,6 +51,12 @@ public class StreamableMacroFutureTask implements Callable<String>
         return renderValue();
     }
 
+    /**
+     * Render Jira element to html directly without threading support
+     * as the plan StreamableMacroFutureTask will not execute in threading pool and will be removed in future,
+     * after implement asynchronous for count
+     * @return String html
+     */
     public String renderValue()
     {
         final long remainingTimeout = context.getTimeout().getTime();
