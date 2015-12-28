08c1c80ef9a279aa35d058db313a8b2acc564662
tdang <tdang@atlassian.com>
2015-12-28 18:50:15 +0700

    CONFDEV-39375: smarter resource loading for jim (cherry picked from commit a682ffe)

--- confluence-jira-plugin/src/main/resources/jira/jira-issues-view-mode/lazy-loading.js
+++ confluence-jira-plugin/src/main/resources/jira/jira-issues-view-mode/lazy-loading.js
@@ -171,28 +171,34 @@ define('confluence/jim/jira/jira-issues-view-mode/lazy-loading', [
         }
     };
 
+    // prepare data element for table placeholder
+    var jimResource = {
+        loadTableResourceIfNeeded: function() {
+            var resDefer = $.Deferred();
+            var $jiraTableIssues = $('.wiki-content .jira-table[data-jira-key]');
+
+            if ($jiraTableIssues.length) {
+                WRM.require(WEB_RESOURCE_TABLE, function() {
+                    jiraRefreshTableMacro = require('confluence/jim/jira/jira-issues-view-mode/refresh-table');
+                    jiraRefreshTableMacro.init();
+                    resDefer.resolve();
+                });
+            } else {
+                resDefer.resolve();
+            }
+
+            return resDefer.promise();
+        }
+    };
+
     var exportModule = {
         /**
          * Initialize the module
          * @return {Object} a Promise object
          */
-        init: function() {
-            var dfd = null;
-
-            // prepare data element for table placeholder
-            var $jiraTableIssues = $('.wiki-content .jira-table[data-jira-key]');
-            if ($jiraTableIssues.length) {
-                dfd = WRM.require(WEB_RESOURCE_TABLE)
-                    .pipe(function() {
-                        jiraRefreshTableMacro = require('confluence/jim/jira/jira-issues-view-mode/refresh-table');
-                        jiraRefreshTableMacro.init();
-                        return core.loadOneByOneJiraServerStrategy();
-                    });
-            } else {
-                dfd = core.loadOneByOneJiraServerStrategy();
-            }
-
-            return dfd;
+        init: function($jiraIssuesElement) {
+            return $.when(jimResource.loadTableResourceIfNeeded())
+                .pipe(core.loadOneByOneJiraServerStrategy);
         }
     };
 
