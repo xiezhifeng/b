fa906cdaf2b286988768b1961f7ec12aa7114120
tdang <tdang@atlassian.com>
2015-12-28 15:34:26 +0700

    CONFDEV-39375: optimize js/css resource loading for jim

--- confluence-jira-plugin/src/main/resources/jira/jira-issues-view-mode/lazy-loading.js
+++ confluence-jira-plugin/src/main/resources/jira/jira-issues-view-mode/lazy-loading.js
@@ -2,16 +2,17 @@ define('confluence/jim/jira/jira-issues-view-mode/lazy-loading', [
     'jquery',
     'ajs',
     'underscore',
-    'confluence/jim/jira/jira-issues-view-mode/fetching-job',
-    'confluence/jim/jira/jira-issues-view-mode/refresh-table'
+    'confluence/jim/jira/jira-issues-view-mode/fetching-job'
 ], function(
     $,
     AJS,
     _,
-    FetchingJob,
-    jiraRefreshTableMacro
+    FetchingJob
 ) {
     'use strict';
+    var WEB_RESOURCE_TABLE = "wr!confluence.extra.jira:refresh-resources";
+    var jiraRefreshTableMacro;
+
 
     // list of jQuery DOM object of all single JIM in page
     var $jiraIssuesEls = null;
@@ -167,7 +168,22 @@ define('confluence/jim/jira/jira-issues-view-mode/lazy-loading', [
          */
         init: function() {
             $jiraIssuesEls = $('.wiki-content [data-jira-key][data-client-id]');
-            return core.loadOneByOneJiraServerStrategy();
+            var dfd = null;
+
+            // prepare data element for table placeholder
+            var $jiraTableIssues = $('.wiki-content .jira-table[data-jira-key]');
+            if ($jiraTableIssues.length) {
+                dfd = WRM.require(WEB_RESOURCE_TABLE)
+                    .pipe(function() {
+                        jiraRefreshTableMacro = require('confluence/jim/jira/jira-issues-view-mode/refresh-table');
+                        jiraRefreshTableMacro.init();
+                        return core.loadOneByOneJiraServerStrategy();
+                    });
+            } else {
+                dfd = core.loadOneByOneJiraServerStrategy();
+            }
+
+            return dfd;
         }
     };
 
