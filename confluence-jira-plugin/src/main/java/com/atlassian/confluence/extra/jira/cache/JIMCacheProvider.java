91c37a840f35467ba4ffc98d64671308cd1473fa
Maksym Fedoryshyn <mfedoryshyn@atlassian.com>
2016-02-22 17:13:47 +1100

    CONFDEV-40735: added javadocs

--- confluence-jira-plugin/src/main/java/com/atlassian/confluence/extra/jira/cache/JIMCacheProvider.java
+++ confluence-jira-plugin/src/main/java/com/atlassian/confluence/extra/jira/cache/JIMCacheProvider.java
@@ -7,15 +7,25 @@ import com.atlassian.vcache.marshallers.MarshallerFactory;
 
 import java.time.Duration;
 
+import javax.annotation.concurrent.ThreadSafe;
+
 /**
- *
+ * Cache provider class  provides helper methods to get different caches in a thread-safe way (to avoid race
+ * conditions on cache creation).
  */
 @Internal
+@ThreadSafe
 public class JIMCacheProvider
 {
     private static final String JIM_CACHE_NAME = "com.atlassian.confluence.extra.jira.JiraIssuesMacro";
     private static final String JIM_INSTANCE_CACHE_NAME = JIM_CACHE_NAME + ".instance";
 
+    /**
+     * Creates new or returns an existent cache
+     *
+     * @param vcacheFactory {@link com.atlassian.vcache.VCacheFactory}
+     * @return new <code>DirectExternalCache</code> or existent one (if was already created)
+     */
     public static synchronized DirectExternalCache<CompressingStringCache> getCache(VCacheFactory vcacheFactory)
     {
         return vcacheFactory.getDirectExternalCache(JIM_CACHE_NAME,
@@ -23,6 +33,12 @@ public class JIMCacheProvider
                 new ExternalCacheSettingsBuilder().build());
     }
 
+    /**
+     * Creates new or returns an existent cache
+     *
+     * @param vcacheFactory {@link com.atlassian.vcache.VCacheFactory}
+     * @return new <code>JvmCache</code> or existent one (if was already created)
+     */
     public static synchronized JvmCache<CacheKey, JiraResponseHandler> getInstanceCache(VCacheFactory vcacheFactory)
     {
         return vcacheFactory.getJvmCache(JIM_INSTANCE_CACHE_NAME,
