294514cea852e8e310891fc19bcbe007e38e779f
Maksym Fedoryshyn <mfedoryshyn@atlassian.com>
2016-02-24 02:06:24 +1100

    CONFDEV-40735: cache updated (cherry picked from commit 2888480)

--- confluence-jira-plugin/src/main/java/com/atlassian/confluence/extra/jira/CacheJiraIssuesManager.java
+++ confluence-jira-plugin/src/main/java/com/atlassian/confluence/extra/jira/CacheJiraIssuesManager.java
@@ -13,22 +13,27 @@ import com.atlassian.sal.api.net.Request.MethodType;
 import com.atlassian.sal.api.net.ResponseException;
 import com.atlassian.util.concurrent.Lazy;
 import com.atlassian.util.concurrent.Supplier;
+import com.atlassian.vcache.DirectExternalCache;
 import com.atlassian.vcache.JvmCache;
+import com.atlassian.vcache.PutPolicy;
 import com.atlassian.vcache.VCacheFactory;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.io.IOException;
 import java.util.List;
+import java.util.Optional;
 
 import static com.atlassian.confluence.extra.jira.util.JiraUtil.JIRA_PLUGIN_KEY;
+import static com.atlassian.vcache.VCacheUtils.join;
 
 public class CacheJiraIssuesManager extends DefaultJiraIssuesManager
 {
 
     private static final Logger log = LoggerFactory.getLogger(CacheJiraIssuesManager.class);
 
-    private final JvmCache<CacheKey, JiraResponseHandler> responseCache;
+    private final DirectExternalCache<JiraChannelResponseHandler> responseChannelHandlerCache;
+    private final DirectExternalCache<JiraStringResponseHandler> responseStringHandlerCache;
     private final Supplier<String> version;
 
     public CacheJiraIssuesManager(JiraIssuesColumnManager jiraIssuesColumnManager,
@@ -36,7 +41,8 @@ public class CacheJiraIssuesManager extends DefaultJiraIssuesManager
             VCacheFactory vcacheFactory, PluginAccessor pluginAccessor)
     {
         super(jiraIssuesColumnManager, jiraIssuesUrlManager, httpRetrievalService);
-        this.responseCache = JIMCacheProvider.getResponseHandlersCache(vcacheFactory);
+        this.responseChannelHandlerCache = JIMCacheProvider.getChannelResponseHandlersCache(vcacheFactory);
+        this.responseStringHandlerCache = JIMCacheProvider.getStringResponseHandlersCache(vcacheFactory);
         this.version = Lazy.supplier(() -> pluginAccessor.getPlugin(JIRA_PLUGIN_KEY).getPluginInformation().getVersion());
     }
 
@@ -61,16 +67,11 @@ public class CacheJiraIssuesManager extends DefaultJiraIssuesManager
 
         final CacheKey mappedCacheKey = new CacheKey(url, appLink.getId().toString(), columns, false, forceAnonymous,
                 false, true, version.get());
+        final CacheKey unmappedCacheKey = new CacheKey(url, appLink.getId().toString(), columns, false,
+                forceAnonymous, false, false, version.get());
 
-        JiraResponseHandler cachedResponseHandler = responseCache.get(mappedCacheKey).orElseGet(() -> {
-            if (userIsMapped == false)
-            {
-                CacheKey unmappedCacheKey = new CacheKey(url, appLink.getId().toString(), columns, false,
-                        forceAnonymous, false, false, version.get());
-                return responseCache.get(unmappedCacheKey).orElse(null);
-            }
-            return null;
-        });
+        JiraResponseHandler cachedResponseHandler = tryToFindResponseHandlerInAllCaches(mappedCacheKey,
+                unmappedCacheKey, userIsMapped);
 
         // Neither mapped cache nor unmapped cache available, request JIRA for data
         if (cachedResponseHandler == null)
@@ -80,11 +81,53 @@ public class CacheJiraIssuesManager extends DefaultJiraIssuesManager
             log.debug("building cache: " + cacheKey);
             JiraResponseHandler responseHandler = super.retrieveXML(url, columns, appLink, forceAnonymous, isAnonymous,
                     handlerType, useCache);
-            responseCache.put(cacheKey, responseHandler);
+            populateCache(cacheKey, responseHandler);
             return responseHandler;
         }
         log.debug("returning cached version");
         return cachedResponseHandler;
+    }
+
+    private JiraResponseHandler tryToFindResponseHandlerInAllCaches(CacheKey mappedCacheKey, CacheKey
+            unmappedCacheKey, boolean userIsMapped)
+    {
+        JiraResponseHandler responseHandler = tryCache(mappedCacheKey, unmappedCacheKey, userIsMapped,
+                responseChannelHandlerCache);
+        if(responseHandler == null)
+        {
+            responseHandler = tryCache(mappedCacheKey, unmappedCacheKey, userIsMapped, responseStringHandlerCache);
+        }
+
+        return responseHandler;
+    }
+
+    private static <T extends JiraResponseHandler> T tryCache(CacheKey mappedCacheKey,
+        CacheKey unmappedCacheKey, boolean userIsMapped, DirectExternalCache<T> cache)
+    {
+        return join(cache.get(mappedCacheKey.toKey())).orElseGet(() -> {
+            if (!userIsMapped)
+            {
+                return join(cache.get(unmappedCacheKey.toKey())).orElse(null);
+            }
+            return null;
+        });
+    }
+
+    private void populateCache(CacheKey cacheKey, JiraResponseHandler responseHandler)
+    {
+        if (responseHandler instanceof JiraChannelResponseHandler)
+        {
+            join(responseChannelHandlerCache.put(cacheKey.toKey(), (JiraChannelResponseHandler) responseHandler, PutPolicy.ADD_ONLY));
+        }
+        else if (responseHandler instanceof JiraStringResponseHandler)
+        {
+            join(responseStringHandlerCache.put(cacheKey.toKey(), (JiraStringResponseHandler) responseHandler, PutPolicy.ADD_ONLY));
+        }
+        else
+        {
+            throw new IllegalArgumentException("Cached value should be either JiraChannelResponseHandler or "
+                    + "JiraStringResponseHandler. " + responseHandler.getClass().getName() + " is not supported.");
+        }
     }
 
 }
