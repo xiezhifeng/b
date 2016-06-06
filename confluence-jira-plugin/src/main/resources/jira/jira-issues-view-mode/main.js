b033c692fa393bfb5e59ebd542a24fb5408d28ea
Quan Pham <qpham@atlassian.com>
2016-06-06 15:03:45 +1000

    CONFDEV-43956: Hacking for listening new global event from Inline Comment for rendering asynchronously Jira Issues Macro inside Inline Comment

--- confluence-jira-plugin/src/main/resources/jira/jira-issues-view-mode/main.js
+++ confluence-jira-plugin/src/main/resources/jira/jira-issues-view-mode/main.js
@@ -26,4 +26,23 @@ require([
             });
         });
     });
+
+    //This a hack at rendering Inline Comments asynchronously
+    $(document).bind("ic-jim-async-supported", function() {
+        var $jiraIssuesEls = $('.wiki-content [data-jira-key][data-client-id]');
+        if ($jiraIssuesEls.length == 0) {
+            return false;
+        }
+
+        WRM.require(WEB_JIM_RESOURCE, function() {
+            require([
+                'confluence/jim/jira/jira-issues-view-mode/lazy-loading',
+                'confluence/jim/jira/jira-issues-view-mode/fix-ui'
+            ], function(JiraIssuesLazyLoading, JiraIssuesFixUI) {
+                JiraIssuesLazyLoading.init($jiraIssuesEls).done(function() {
+                    JiraIssuesFixUI.fixBreakIconInOldConf();
+                });
+            });
+        });
+    });
 });
