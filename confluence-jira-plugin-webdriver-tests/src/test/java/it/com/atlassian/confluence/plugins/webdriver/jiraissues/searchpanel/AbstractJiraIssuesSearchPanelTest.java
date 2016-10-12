8c302bf1356e2514fb2d621a794b4d290d33c04d
Alex Yakovlev <alyakovlev@atlassian.com>
2016-10-12 09:09:06 -0700

    CONFDEV-48209 Login and edit for each test to see if we can get stability

--- confluence-jira-plugin-webdriver-tests/src/test/java/it/com/atlassian/confluence/plugins/webdriver/jiraissues/searchpanel/AbstractJiraIssuesSearchPanelTest.java
+++ confluence-jira-plugin-webdriver-tests/src/test/java/it/com/atlassian/confluence/plugins/webdriver/jiraissues/searchpanel/AbstractJiraIssuesSearchPanelTest.java
@@ -28,44 +28,17 @@ public abstract class AbstractJiraIssuesSearchPanelTest extends AbstractJiraTest
     protected static final List<String> LIST_URL_TEST_COLUMN = ImmutableList.of("Summary", "Issue Type", "URL", "Key");
     protected static List<String> LIST_DEFAULT_COLUMN = ImmutableList.of("Key", "Summary", "Issue Type", "Created", "Updated", "Due Date", "Assignee", "Reporter", "Priority", "Status", "Resolution");
 
-    @BeforeClass
-    public static void init() throws Exception
-    {
-        editPage = gotoEditTestPage(user.get());
-    }
-
     @Before
     public void setup() throws Exception
     {
-        if (editPage == null)
-        {
-            editPage = gotoEditTestPage(user.get());
-        }
-        else
-        {
-            if (editPage.getEditor().isCancelVisibleNow())
-            {
-                // in editor page.
-                editPage.getEditor().getContent().clear();
-            }
-            else
-            {
-                // in view page, and then need to go to edit page.
-                editPage = gotoEditTestPage(user.get());
-            }
-        }
+        editPage = gotoEditTestPage(user.get());
     }
 
     @After
     public void closeJiraSearchDialog() throws Exception
     {
-        closeDialog(jiraMacroSearchPanelDialog);
-    }
-
-    @AfterClass
-    public static void clean() throws Exception
-    {
-        cancelEditPage(editPage);
+        product.logOut();
+        //closeDialog(jiraMacroSearchPanelDialog);
     }
 
     protected JiraMacroSearchPanelDialog openJiraIssueSearchPanelAndStartSearch(String searchValue) throws Exception
