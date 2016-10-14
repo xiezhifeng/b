50a7b9b784e30b926783fbfbf5b4aed4eeea5e6f
Alex Yakovlev <alyakovlev@atlassian.com>
2016-10-13 21:36:55 -0700

    CONFDEV-48209 Move the failing test to its own class

--- confluence-jira-plugin-webdriver-tests/src/test/java/it/com/atlassian/confluence/plugins/webdriver/jiraissues/searchpanel/pageview/JiraIssuesSearch.java
+++ confluence-jira-plugin-webdriver-tests/src/test/java/it/com/atlassian/confluence/plugins/webdriver/jiraissues/searchpanel/pageview/JiraIssuesSearch.java
@@ -1,6 +1,5 @@
 package it.com.atlassian.confluence.plugins.webdriver.jiraissues.searchpanel.pageview;
 
-import com.atlassian.confluence.webdriver.pageobjects.page.content.EditContentPage;
 import com.atlassian.pageobjects.elements.query.Poller;
 import it.com.atlassian.confluence.plugins.webdriver.helper.ApplinkHelper;
 import it.com.atlassian.confluence.plugins.webdriver.jiraissues.searchpanel.AbstractJiraIssuesSearchPanelTest;
@@ -25,43 +24,6 @@ public class JiraIssuesSearch extends AbstractJiraIssuesSearchPanelTest
             ApplinkHelper.deleteApplink(client, globalTestAppLinkId, getAuthQueryString());
         }
         globalTestAppLinkId = "";
-    }
-
-    @Test
-    public void testColumnNotSupportSortableInIssueTable() throws Exception
-    {
-        jiraMacroSearchPanelDialog = openJiraIssueSearchPanelDialogFromMacroBrowser(editPage);
-        jiraMacroSearchPanelDialog.inputJqlSearch("status = open");
-        jiraMacroSearchPanelDialog.clickSearchButton();
-        jiraMacroSearchPanelDialog.openDisplayOption();
-        jiraMacroSearchPanelDialog.getDisplayOptionPanel().addColumn("Linked Issues");
-        jiraMacroSearchPanelDialog.clickInsertDialog();
-        editPage.getEditor().getContent().waitForInlineMacro(JIRA_ISSUE_MACRO_NAME);
-        editPage.getEditor().clickSaveAndWaitForPageChange();
-        JiraIssuesPage page = pageBinder.bind(JiraIssuesPage.class);
-        String keyValueAtFirstTime = page.getFirstRowValueOfSummay();
-        page.clickColumnHeaderIssueTable("Linked Issues",null);
-        String keyAfterSort = page.getFirstRowValueOfSummay();
-        assertEquals(keyValueAtFirstTime, keyAfterSort);
-    }
-
-    @Test
-    public void testPasteXmlUrl() throws Exception
-    {
-        JiraIssuesPage jiraIssuesPage = createPageWithJiraIssueMacro(JIRA_DISPLAY_URL + "/si/jira.issueviews:issue-xml/TST-1/TST-1.xml", true);
-        Poller.waitUntilTrue(jiraIssuesPage.isSingleContainText("Test bug"));
-    }
-
-    @Test
-    public void testPasteUrlWithJiraServer() throws Exception
-    {
-        //create another primary applink
-        String jiraURL = "http://jira.test.com";
-        String authArgs = getAuthQueryString();
-        globalTestAppLinkId = ApplinkHelper.createAppLink(client, "TEST", authArgs, jiraURL, jiraURL, true);
-
-        JiraIssuesPage jiraIssuesPage = createPageWithJiraIssueMacro(JIRA_DISPLAY_URL + "/browse/TST-1", true);
-        Poller.waitUntilTrue(jiraIssuesPage.isSingleContainText("Test bug"));
     }
 
     @Test
