34887f29d8ac240fa5be2894f09c5c258594bf1c
Hasnae Rehioui <hrehioui@atlassian.com>
2016-10-19 15:21:54 +1100

    CONFDEV-48209: bye bye old classes, welcome new tests

--- confluence-jira-plugin-webdriver-tests/src/test/java/it/com/atlassian/confluence/plugins/webdriver/jiracharts/macrobrowser/TwoDimensionalChartDialogTest.java
+++ confluence-jira-plugin-webdriver-tests/src/test/java/it/com/atlassian/confluence/plugins/webdriver/jiracharts/macrobrowser/TwoDimensionalChartDialogTest.java
@@ -9,7 +9,7 @@ import org.junit.Test;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
 
-public class TwoDimensionalChartDialog extends AbstractJiraIssueMacroChartTest
+public class TwoDimensionalChartDialogTest extends AbstractJiraIssueMacroChartTest
 {
 
     @Test
--- confluence-jira-plugin-webdriver-tests/src/test/java/it/com/atlassian/confluence/plugins/webdriver/jiracharts/pageview/TwoDimensionalChartDialogViewTest.java
+++ confluence-jira-plugin-webdriver-tests/src/test/java/it/com/atlassian/confluence/plugins/webdriver/jiracharts/pageview/TwoDimensionalChartDialogViewTest.java
@@ -1,14 +1,13 @@
 package it.com.atlassian.confluence.plugins.webdriver.jiracharts.pageview;
 
 
-import it.com.atlassian.confluence.plugins.webdriver.jiracharts.AbstractJiraChartTest;
+import it.com.atlassian.confluence.plugins.webdriver.jiracharts.AbstractJiraIssueMacroChartTest;
 import it.com.atlassian.confluence.plugins.webdriver.pageobjects.jirachart.JiraChartViewPage;
-
 import org.junit.Test;
 
 import static org.junit.Assert.assertTrue;
 
-public class TwoDimensionalChartDialog extends AbstractJiraChartTest
+public class TwoDimensionalChartDialogViewTest extends AbstractJiraIssueMacroChartTest
 {
     @Test
     public void testTwoDimensionalChartShowMore() throws InterruptedException
@@ -23,19 +22,23 @@ public class TwoDimensionalChartDialog extends AbstractJiraChartTest
         dialogTwoDimensionalChart.clickPreviewButton();
         assertTrue(dialogTwoDimensionalChart.isChartImageVisible());
 
-        editPage = dialogTwoDimensionalChart.clickInsertDialog();
-        editPage.getEditor().getContent().waitForInlineMacro(JIRA_CHART_MACRO_NAME);
-        editPage.getEditor().clickSaveAndWaitForPageChange();
+        editContentPage = dialogTwoDimensionalChart.clickInsertDialog();
+        editContentPage.getEditor().getContent().waitForInlineMacro(JIRA_CHART_MACRO_NAME);
+        editContentPage.getEditor().clickSaveAndWaitForPageChange();
 
-        pageJiraChartView = pageBinder.bind(JiraChartViewPage.class);
-        assertTrue(pageJiraChartView.getChartSummary().getText(), pageJiraChartView.getChartSummary().getText().contains("Showing 1 of 2 statistics"));
+        JiraChartViewPage pageJiraChartView = pageBinder.bind(JiraChartViewPage.class);
+        assertTrue(
+                pageJiraChartView.getChartSummary().getText(),
+                pageJiraChartView.getChartSummary().getText().contains("Showing 1 of 2 statistics")
+        );
         assertTrue(pageJiraChartView.getXAxis().equals("Status"));
         assertTrue(pageJiraChartView.getYAxis().equals("Issue Type"));
-
         assertTrue(pageJiraChartView.getShowLink().getText().contains("Show more"));
+
         pageJiraChartView.clickShowLink();
         assertTrue(pageJiraChartView.getChartSummary().getText().contains("Showing 2 of 2 statistics"));
         assertTrue(pageJiraChartView.getShowLink().getText().contains("Show less"));
+
         pageJiraChartView.clickShowLink();
         assertTrue(pageJiraChartView.getChartSummary().getText().contains("Showing 1 of 2 statistics"));
     }
